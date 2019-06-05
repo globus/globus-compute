46eaae30d81cddc8f56b51913848362c047124d8
Anna Elizabeth Woodard <awoodard@nd.edu>
2019-06-04 21:52:03 -0500

    Remove more parsl-specific files

--- funcx/executor/parsl/channels/__init__.py
+++
@@ -1,5 +0,0 @@
-from parsl.channels.ssh.ssh import SSHChannel
-from parsl.channels.local.local import LocalChannel
-from parsl.channels.ssh_il.ssh_il import SSHInteractiveLoginChannel
-
-__all__ = ['SSHChannel', 'LocalChannel', 'SSHInteractiveLoginChannel']
