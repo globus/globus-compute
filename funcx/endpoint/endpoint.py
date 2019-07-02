a1cdce384692a9697b4e325c130ae5ff77812b37
Ryan <rchard@anl.gov>
2019-07-01 20:49:00 -0500

    Adding try catch to exec

--- funcx/endpoint/endpoint.py
+++ funcx/endpoint/endpoint.py
@@ -25,10 +25,14 @@ parsl.load(_get_parsl_config())
 dfk = parsl.dfk()
 ex = dfk.executors['htex_local']
 
+
 @python_app
 def run_code(code, entry_point, event=None):
-    exec(code)
-    return eval(entry_point)(event)
+    try:
+        exec(code)
+        return eval(entry_point)(event)
+    except Exception as e:
+        return str(e)
 
 
 def send_request(serving_url, inputs):
@@ -56,7 +60,6 @@ def server(ip, port):
     write_option("endpoint_uuid", endpoint_uuid)
 
     endpoint_worker = ZMQWorker("tcp://{}:{}".format(ip, port), endpoint_uuid)
-    reply = None
     task_q = queue.Queue()
     result_q = queue.Queue()
     threads = []
@@ -83,13 +86,13 @@ def result_worker(endpoint_worker, result_q):
         (result, reply_to)= result_q.get()
         endpoint_worker.send(result, reply_to)
 
+
 def parsl_worker(task_q, result_q):
     exec_times = []
     endpoint_times = []
     while True:
         if task_q:
             request, reply_to = task_q.get()
-            t0 = time.time()
             to_do = pickle.loads(request[0])
             code, entry_point, event = to_do[-1]['function'], to_do[-1]['entry_point'], to_do[-1]['event']
             result = pickle.dumps(run_code(code, entry_point, event=event).result())
@@ -98,6 +101,7 @@ def parsl_worker(task_q, result_q):
 
 def main():
     server('funcX.org', 50001)
+
 
 if __name__ == "__main__":
     main()
