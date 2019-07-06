6397b038d7780d684babb0e87b51094cd6e8df7f
Ryan <rchard@anl.gov>
2019-07-06 13:57:00 -0500

    Fixing endpoint registration address for hub.

--- funcx/endpoint/endpoint.py
+++ funcx/endpoint/endpoint.py
@@ -9,7 +9,7 @@ import queue
 
 from funcx.sdk.client import FuncXClient
 from funcx.endpoint.utils.zmq_worker import ZMQWorker
-from funcx.endpoint.config import (_get_parsl_config, _load_auth_client, _get_executor)
+from funcx.endpoint.config import (_get_parsl_config, _load_auth_client, _get_executor, FUNCX_HUB_URL)
 from funcx.sdk.config import lookup_option, write_option
 
 from funcx.executor import HighThroughputExecutor
@@ -51,7 +51,7 @@ def execute_function(code, entry_point, event=None):
 class FuncXEndpoint:
 
     def __init__(self,
-                 ip="funcx.org",
+                 ip=FUNCX_HUB_URL,
                  port=50001,
                  worker_threads=1,
                  container_type="Singularity",
@@ -206,7 +206,7 @@ class FuncXEndpoint:
 
 def start_endpoint():
     logging.debug("Starting endpoint")
-    ep = FuncXEndpoint(ip='funcX.org', port=50001, container_type='docker')
+    ep = FuncXEndpoint(ip=FUNCX_HUB_URL, port=50001, container_type='docker')
 
 
 if __name__ == "__main__":
