4d9447bb27e2377bda7675282affce1615bc1307
Yadu Nand B <yadudoc1729@gmail.com>
2019-07-11 13:04:53 -0500

    Adding a preliminary redis queue implementation

---
+++ funcx/queues/redis/redis_q.py
@@ -0,0 +1,93 @@
+import redis
+from funcx.queues.base import FuncxQueue, NotConnected
+import json
+
+
+class RedisQueue(FuncxQueue):
+    """ A basic redis queue
+
+    The queue only connects when the `connect` method is called to avoid
+    issues with passing an object across processes.
+
+    Parameters
+    ----------
+
+    hostname : str
+       Hostname of the redis server
+
+    port : int
+       Port at which the redis server can be reached. Default: 6379
+
+    """
+
+    def __init__(self, hostname, port=6379):
+        """ Initialize
+        """
+        self.hostname = hostname
+        self.port = port
+        self.redis_client = None
+
+    def connect(self):
+        """ Connects to the Redis server
+        """
+        try:
+            if not self.redis_client:
+                self.redis_client = redis.StrictRedis(host=self.hostname, port=self.port, decode_responses=True)
+        except redis.exceptions.ConnectionError:
+            print(f"ConnectionError while trying to connect to Redis@{self.hostname}:{self.port}")
+            raise
+
+    def get(self, timeout=1):
+        """ Get an item from the redis queue
+
+        Parameters
+        ----------
+        timeout : int
+           Timeout for the blocking get in seconds
+        """
+        try:
+            task_list, task_id = self.redis_client.blpop('task_list', timeout=timeout)
+            jtask_info = self.redis_client.get(f'task:{task_id}')
+            task_info = json.loads(jtask_info)
+        except AttributeError:
+            raise NotConnected(self)
+        except redis.exceptions.ConnectionError:
+            print(f"ConnectionError while trying to connect to Redis@{self.hostname}:{self.port}")
+            raise
+
+        return task_id, task_info
+
+
+    def put(self, key, payload):
+        """ Put's the key:payload into a dict and pushes the key onto a queue
+        Parameters
+        ----------
+        key : str
+            The task_id to be pushed
+
+        payload : dict
+            Dict of task information to be stored
+        """
+        try:
+            self.redis_client.set(f'task:{key}', json.dumps(payload))
+            self.redis_client.rpush('task_list', key)
+        except AttributeError:
+            raise NotConnected(self)
+        except redis.exceptions.ConnectionError:
+            print(f"ConnectionError while trying to connect to Redis@{self.hostname}:{self.port}")
+            raise
+
+    @property
+    def is_connected(self):
+        return self.redis_client != None
+
+
+def test():
+    rq = RedisQueue('127.0.0.1')
+    rq.connect()
+    rq.put("01", {'a':1, 'b':2})
+    res = rq.get(timeout=1)
+    print("Result : ", res)
+
+if __name__ == '__main__':
+    test()
