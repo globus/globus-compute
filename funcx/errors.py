d70fef0034ef49b7ae7a3bd72bd1dacd8e0d148b
Yadu Nand B <yadudoc1729@gmail.com>
2019-07-16 23:02:21 -0500

    Adding some exceptions

--- funcx/errors.py
+++ funcx/errors.py
@@ -2,3 +2,18 @@
 class FuncxError(Exception):
     """ Base class for all funcx exceptions
     """
+
+    def __str__(self):
+        return self.__repr__
+
+
+class RegistrationError(FuncxError):
+    """ Registering the endpoint has failed
+    """
+
+    def __init__(self, reason):
+        self.reason = reason
+
+    def __repr__(self):
+        return "Endpoint registration failed due to {}".format(self.reason)
+
