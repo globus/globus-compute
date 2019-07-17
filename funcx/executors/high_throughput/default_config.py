035aad0ef5277121bb29ae3f7e8e8f87311455ba
Yadu Nand B <yadudoc1729@gmail.com>
2019-07-17 16:59:57 -0500

    Updating config loading mechanism in the interchange

--- funcx/executors/high_throughput/default_config.py
+++ funcx/executors/high_throughput/default_config.py
@@ -1,10 +1,3 @@
-from funcx.executors import HighThroughputExecutor
-from parsl.providers import LocalProvider
-from parsl.channels import LocalChannel
+from funcx.config import Config
 
-# Create a config object
-config = HighThroughputExecutor(
-    provider=LocalProvider(
-        channel=LocalChannel
-    )
-)
+config = Config()
