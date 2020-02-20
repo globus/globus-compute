c63538e57919e00bc9140bebc18378f30f824723
Yadu Nand B <yadudoc1729@gmail.com>
2020-02-20 10:52:03 -0600

    New get_batch_status and update_table.
    * Fixes to handling exception as results.
    * get_batch_result is a stub

--- funcx/sdk/client.py
+++ funcx/sdk/client.py
@@ -3,6 +3,8 @@ import os
 import logging
 import pickle as pkl
 
+from parsl.app.errors import RemoteExceptionWrapper
+
 from fair_research_login import NativeClient, JSONTokenStorage
 from funcx.serialize import FuncXSerializer
 # from funcx.sdk.utils.futures import FuncXFuture
@@ -44,6 +46,7 @@ class FuncXClient(throttling.ThrottledBaseClient):
 
         Keyword arguments are the same as for BaseClient.
         """
+        self.func_table = {}
         self.ep_registration_path = 'register_endpoint_2'
         self.funcx_home = os.path.expanduser(funcx_home)
 
@@ -80,6 +83,47 @@ class FuncXClient(throttling.ThrottledBaseClient):
         """
         self.native_client.logout()
 
+
+    def update_table(self, return_msg, task_id):
+        """ Parses the return message from the service and updates the internal func_tables
+
+        Parameters
+        ----------
+
+        return_msg : str
+           Return message received from the funcx service
+        task_id : str
+           task id string
+        """
+        if isinstance(return_msg, str):
+            r_dict = json.loads(return_msg)
+        else:
+            r_dict = return_msg
+
+        status = {'pending': True}
+
+        if 'result' in r_dict:
+            try:
+                r_obj = self.fx_serializer.deserialize(r_dict['result'])
+            except Exception:
+                raise Exception("Failure during deserialization of the result object")
+            else:
+                status.update({'pending': 'False',
+                               'result': r_obj})
+                self.func_table[task_id] = status
+
+        elif 'exception' in r_dict:
+            try:
+                r_exception = self.fx_serializer.deserialize(r_dict['exception'])
+                logger.info(f"Exception : {r_exception}")
+            except Exception:
+                raise Exception("Failure during deserialization of the Task's exception object")
+            else:
+                status.update({'pending': 'False',
+                               'exception': r_exception})
+                self.func_table[task_id] = status
+        return status
+
     def get_task_status(self, task_id):
         """Get the status of a funcX task.
 
@@ -93,9 +137,16 @@ class FuncXClient(throttling.ThrottledBaseClient):
         dict
             Status block containing "status" key.
         """
+        if task_id in self.func_table:
+             return self.func_table[task_id]
 
         r = self.get("{task_id}/status".format(task_id=task_id))
-        return json.loads(r.text)
+        logger.debug("Response string : {}".format(r))
+        try:
+            rets = self.update_table(r.text, task_id)
+        except Exception as e:
+            raise e
+        return rets
 
     def get_result(self, task_id):
         """ Get the result of a funcX task
@@ -113,31 +164,51 @@ class FuncXClient(throttling.ThrottledBaseClient):
         ------
         Exception obj: Exception due to which the task failed
         """
-
-        r = self.get("{task_id}/status".format(task_id=task_id))
-
-        logger.info(f"Got from globus : {r}")
-        r_dict = json.loads(r.text)
-
-        if 'result' in r_dict:
-            try:
-                r_obj = self.fx_serializer.deserialize(r_dict['result'])
-            except Exception:
-                raise Exception("Failure during deserialization of the result object")
-            else:
-                return r_obj
-
-        elif 'exception' in r_dict:
-            try:
-                r_exception = self.fx_serializer.deserialize(r_dict['exception'])
-                logger.info(f"Exception : {r_exception}")
-            except Exception:
-                raise Exception("Failure during deserialization of the Task's exception object")
-            else:
-                r_exception.reraise()
-
-        else:
+        status = self.get_task_status(task_id)
+        if status['pending'] is True:
             raise Exception("Task pending")
+        else:
+            if 'result' in status:
+                return status['result']
+            else:
+                logger.warn("We have an exception : {}".format(status['exception']))
+                status['exception'].reraise()
+
+    def get_batch_status(self, task_id_list):
+        """ Request status for a batch of task_ids
+        """
+        assert isinstance(task_id_list, list), "get_batch_status expects a list of task ids"
+
+
+        pending_task_ids = [t for t in task_id_list if t not in self.func_table]
+
+        results = {}
+
+        if pending_task_ids :
+            payload = {'task_ids': pending_task_ids}
+            r = self.post("/batch_status", json_body=payload)
+            logger.debug("Response string : {}".format(r))
+
+        for task_id in task_id_list:
+            if task_id in pending_task_ids:
+                try:
+                    data = r['results'][task_id]
+                    rets = self.update_table(data, task_id)
+                    results[task_id] = rets
+                except KeyError:
+                    logger.debug("Task {} info was not available in the batch status")
+                except Exception as e:
+                    logger.exception("Failure while unpacking results fom get_batch_status")
+            else:
+                results[task_id] = self.func_table[task_id]
+
+        return results
+
+
+    def get_batch_result(self, task_id_list):
+        """ Request results for a batch of task_ids
+        """
+        pass
 
 
     def run(self, *args, endpoint_id=None, function_id=None, asynchronous=False, **kwargs):
@@ -220,7 +291,7 @@ class FuncXClient(throttling.ThrottledBaseClient):
         batch_payload = []
         iterator = args[0]
         for arg in iterator:
-            ser_args = self.fx_serializer.serialize((arg))
+            ser_args = self.fx_serializer.serialize((arg,))
             payload = self.fx_serializer.pack_buffers([ser_args, ser_kwargs])
             batch_payload.append(payload)
 
