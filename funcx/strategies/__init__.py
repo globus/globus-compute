eadfbd1fe5214e3b2ab1a24bc8031ed6ab3f3a37
Zhuozhao Li <zhuozhl@clemson.edu>
2020-02-25 16:42:32 -0600

    adding a strategy for kube based on task types

--- funcx/strategies/__init__.py
+++ funcx/strategies/__init__.py
@@ -1,5 +1,8 @@
 from funcx.strategies.base import BaseStrategy
 from funcx.strategies.simple import SimpleStrategy
+from funcx.strategies.kube_simple import KubeSimpleStrategy
+
 
 __all__ = ['BaseStrategy',
-           'SimpleStrategy']
+           'SimpleStrategy',
+           'KubeSimpleStrategy']
