e2757b2f490a1814b3b74ac96370dc12a193ba28
ZhuozhaoLi <zhuozhl@clemson.edu>
2020-03-23 08:56:34 -0700

    remove accidentally pushed files

--- cmd_parsl.auto.1584844524.4652069.sh
+++
@@ -1,1 +0,0 @@
-funcx-manager --debug --max_workers=10 -c 1.0 --poll 10 --task_url=tcp://comet-ln3.sdsc.edu:54927 --result_url=tcp://comet-ln3.sdsc.edu:54241 --logdir=/home/zhuozhao/.funcx/covid19-remote/worker_logs --block_id=13 --hb_period=30 --hb_threshold=120 --worker_mode=no_container --scheduler_mode=soft --worker_type=None 
