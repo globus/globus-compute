1ba5a7f980cd555a4265a22dd543fd05686e2405
Ben Galewsky <ben@peartreestudio.net>
2020-08-23 20:14:18 -0500

    Add root of funcx module to installed package

--- funcx_sdk/setup.py
+++ funcx_sdk/setup.py
@@ -12,7 +12,7 @@ with open('requirements.txt') as f:
 setup(
     name='funcx-sdk',
     version=version,
-    packages=find_namespace_packages(include=['funcx.*']),
+    packages=find_namespace_packages(include=["funcx", 'funcx.*']),
     description='funcX: High Performance Function Serving for Science',
     install_requires=install_requires,
     python_requires=">=3.6.0",
