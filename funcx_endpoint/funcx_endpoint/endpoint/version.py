3b240c8bab7b99a41200df21b923111c45e570c1
Ben Galewsky <ben@peartreestudio.net>
2020-09-02 10:23:03 -0500

    Change worker log dir and revert version number

--- funcx_endpoint/funcx_endpoint/endpoint/version.py
+++ funcx_endpoint/funcx_endpoint/endpoint/version.py
@@ -1,6 +1,6 @@
 # single source of truth for package version,
 # see https://packaging.python.org/en/latest/single_source_version/
-__version__ = "0.0.3"
+__version__ = "0.0.2a0"
 VERSION = __version__
 
 # app name to send as part of requests
