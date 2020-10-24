29d769816a3fa2910e35783670fa50b240b74fe6
Ben Glick <glick@glick.cloud>
2020-10-24 13:03:14 -0700

    fix errors

--- funcx_sdk/funcx/sdk/client.py
+++ funcx_sdk/funcx/sdk/client.py
@@ -341,7 +341,7 @@ class FuncXClient(throttling.ThrottledBaseClient):
         r = self.post(servable_path, json_body=data)
         if r.http_status != 200:
             raise HTTPError(r)
-        if r.get("status", "Failure") == "Failure":
+        if r.get("status", "Failed") == "Failed":
             raise MalformedResponse("FuncX Request failed: {}".format(r.get("reason", "Unknown")))
         return r['task_uuids']
 
@@ -387,7 +387,7 @@ class FuncXClient(throttling.ThrottledBaseClient):
         if r.http_status != 200:
             raise Exception(r)
 
-        if r.get("status", "Failure") == "Failure":
+        if r.get("status", "Failed") == "Failed":
             raise MalformedResponse("FuncX Request failed: {}".format(r.get("reason", "Unknown")))
         return r['task_uuids']
 
