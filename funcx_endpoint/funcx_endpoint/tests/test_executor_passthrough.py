130f94f52064ac38e76472c6b19dc9d60dad7500
Yadu Nand Babuji <yadudoc1729@gmail.com>
2020-11-18 01:42:34 -0600

    Updating test to match new passthrough model

--- funcx_endpoint/funcx_endpoint/tests/test_executor_passthrough.py
+++ funcx_endpoint/funcx_endpoint/tests/test_executor_passthrough.py
@@ -4,36 +4,48 @@ from funcx.utils.loggers import set_file_logger
 import uuid
 from funcx.serialize import FuncXSerializer
 from funcx_endpoint.executors.high_throughput.messages import Message, Task
+import time
+import pickle
+from multiprocessing import Queue
 
 def double(x):
     return x * 2
 
 if __name__ == "__main__":
 
+    results_queue = Queue()
     #    set_file_logger('executor.log', name='funcx_endpoint', level=logging.DEBUG)
     htex = HighThroughputExecutor(interchange_local=True,
                                   passthrough=True)
-    htex.start()
+
+    htex.start(results_passthrough=results_queue)
     htex._start_remote_interchange_process()
     fx_serializer = FuncXSerializer()
 
-    results_queue = htex.get_results_passthrough()
 
-    task_id = str(uuid.uuid4())
+    for i in range(10):
+        task_id = str(uuid.uuid4())
+        args = (i,)
+        kwargs = {}
 
-    args = (5,)
-    kwargs = {}
+        fn_code = fx_serializer.serialize(double)
+        ser_code = fx_serializer.pack_buffers([fn_code])
+        ser_params = fx_serializer.pack_buffers([fx_serializer.serialize(args),
+                                                 fx_serializer.serialize(kwargs)])
 
-    fn_code = fx_serializer.serialize(double)
-    ser_code = fx_serializer.pack_buffers([fn_code])
-    ser_params = fx_serializer.pack_buffers([fx_serializer.serialize(args),
-                                             fx_serializer.serialize(kwargs)])
 
-    payload = Task(task_id,
-                   'RAW',
-                   ser_code + ser_params)
-    f = htex.submit_raw(payload.pack())
+        payload = Task(task_id,
+                       'RAW',
+                       ser_code + ser_params)
+        f = htex.submit_raw(payload.pack())
+        time.sleep(0.5)
 
-    result_package = results_queue.get()
-    print("Result package : ", result_package)
-    # print("Result : ", f.result())
+    for i in range(10):
+        result_package = results_queue.get()
+        # print("Result package : ", result_package)
+        r = pickle.loads(result_package)
+        result = fx_serializer.deserialize(r['result'])
+        print(f"Result:{i}: {result}")
+
+
+    print("All done")
