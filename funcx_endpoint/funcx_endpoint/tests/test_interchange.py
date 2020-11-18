1b1751610b195c8717f4a44359af9b9cd89eee25
Yadu Nand Babuji <yadudoc1729@gmail.com>
2020-11-18 01:43:32 -0600

    Updating interchange call

--- funcx_endpoint/funcx_endpoint/tests/test_interchange.py
+++ funcx_endpoint/funcx_endpoint/tests/test_interchange.py
@@ -15,8 +15,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     config = Config()
 
-    ic = Interchange(config,
-                     client_address=args.address,
+    ic = Interchange(client_address=args.address,
                      client_ports=[int(i) for i in args.client_ports.split(',')],
                       )
     ic.start()
