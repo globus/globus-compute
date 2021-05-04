0c103c407d60a56a2747dff0f1412b8c56fc31a1
Yadu Nand Babuji <yadudoc1729@gmail.com>
2021-05-04 11:27:56 -0500

    Adding an isolated function deserialization checking mechanism.
    This change adds:
        * off_process_checker.py: A subprocess'ed server that attempts
        deserialization in a clean isolated env and reports success/fail
        * Updates to concretes.py to prioritize source based fn serialization
        methods that are cross-python-version reliable
        * Updates to facade.py to launch the off_process_checker as a subprocess
        and send it fn's over a socket protected with a mutex. The checker will
        print the port it's listening to on a file.
        * Adding off_process_checker as an installed script to setup.py

--- funcx_sdk/funcx/serialize/concretes.py
+++ funcx_sdk/funcx/serialize/concretes.py
@@ -27,6 +27,52 @@ class pickle_base64(fxPicker_shared):
         return data
 
 
+class code_dill_source(fxPicker_shared):
+
+    _identifier = '04\n'
+    _for_code = True
+
+    def __init__(self):
+        super().__init__()
+
+    def serialize(self, data):
+        name = data.__name__
+        body = dill.source.getsource(data)
+        x = codecs.encode(pickle.dumps((name, body)), 'base64').decode()
+        return self.identifier + x
+
+    def deserialize(self, payload):
+        chomped = self.chomp(payload)
+        name, body = pickle.loads(codecs.decode(chomped.encode(), 'base64'))
+        exec(body)
+        return locals()[name]
+
+
+class code_text_inspect(fxPicker_shared):
+    """ We use dill to get the source code out of the function object
+    and then exec the function body to load it in. The function object
+    is then returned by name.
+    """
+
+    _identifier = '03\n'
+    _for_code = True
+
+    def __init__(self):
+        super().__init__()
+
+    def serialize(self, data):
+        name = data.__name__
+        body = inspect.getsource(data)
+        x = codecs.encode(pickle.dumps((name, body)), 'base64').decode()
+        return self.identifier + x
+
+    def deserialize(self, payload):
+        chomped = self.chomp(payload)
+        name, body = pickle.loads(codecs.decode(chomped.encode(), 'base64'))
+        exec(body)
+        return locals()[name]
+
+
 class code_dill(fxPicker_shared):
     """ We use dill to serialize the function object
     """
@@ -63,31 +109,6 @@ class code_pickle(fxPicker_shared):
         chomped = self.chomp(payload)
         data = pickle.loads(codecs.decode(chomped.encode(), 'base64'))
         return data
-
-
-class code_text_inspect(fxPicker_shared):
-    """ We use dill to get the source code out of the function object
-    and then exec the function body to load it in. The function object
-    is then returned by name.
-    """
-
-    _identifier = '03\n'
-    _for_code = True
-
-    def __init__(self):
-        super().__init__()
-
-    def serialize(self, data):
-        name = data.__name__
-        body = inspect.getsource(data)
-        x = codecs.encode(pickle.dumps((name, body)), 'base64').decode()
-        return self.identifier + x
-
-    def deserialize(self, payload):
-        chomped = self.chomp(payload)
-        name, body = pickle.loads(codecs.decode(chomped.encode(), 'base64'))
-        exec(body)
-        return locals()[name]
 
 
 def bar(x, y={'a': 3}):
--- funcx_sdk/funcx/serialize/facade.py
+++ funcx_sdk/funcx/serialize/facade.py
@@ -1,7 +1,12 @@
+import atexit
+import multiprocessing as mp
+import subprocess
+import uuid
+import time
 from funcx.serialize.concretes import *
 from funcx.serialize.base import METHODS_MAP_DATA, METHODS_MAP_CODE
+from funcx.serialize.off_process_checker import OffProcessClient
 import logging
-
 logger = logging.getLogger(__name__)
 
 
@@ -9,13 +14,28 @@ class FuncXSerializer(object):
     """ Wraps several serializers for one uniform interface
     """
 
-    def __init__(self):
+    def __init__(self, use_offprocess_checker=False):
         """ Instantiate the appropriate classes
         """
 
         # Do we want to do a check on header size here ? Probably overkill
         headers = list(METHODS_MAP_CODE.keys()) + list(METHODS_MAP_DATA.keys())
         self.header_size = len(headers[0])
+        self.use_offprocess_checker = use_offprocess_checker
+
+        if self.use_offprocess_checker:
+            self.serialize_lock = mp.Lock()
+
+            logger.debug("Starting off_process_checker")
+            try:
+                port = self._start_off_process_checker()
+            except Exception:
+                logger.exception("Off_process_checker instantiation failed")
+                self.use_offprocess_checker = False
+            else:
+                self.off_proc_client = OffProcessClient(port)
+                self.off_proc_client.connect()
+                atexit.register(self.cleanup)
 
         self.methods_for_code = {}
         self.methods_for_data = {}
@@ -25,8 +45,56 @@ class FuncXSerializer(object):
         for key in METHODS_MAP_DATA:
             self.methods_for_data[key] = METHODS_MAP_DATA[key]()
 
+    def _start_off_process_checker(self):
+        """ Kicks off off_process_checker using subprocess and returns the port on which it
+        is listening.
+        """
+        std_file = f'/tmp/{uuid.uuid4()}'
+        logger.debug(f"Writing off_process_checker logs to: {std_file}")
+        std_out = open(std_file, 'w')
+        self.serialize_proc = subprocess.Popen(['off_process_checker.py'], stdout=std_out, stderr=subprocess.STDOUT)
+        with open(std_file) as f:
+            for i in range(200, 3000, 200):
+                time.sleep(i / 100)  # Sleep incrementally waiting for process start
+                for line in f.readlines():
+                    if 'BINDING TO' in line:
+                        _, port = line.strip().split(':')
+                        port = int(port)
+                        return port
+
+        raise Exception("Failed to determine off_process_checker's port")
+
     def _list_methods(self):
         return self.methods_for_code, self.methods_for_data
+
+    def cleanup(self):
+        logger.debug("Cleaning up")
+        if self.use_offprocess_checker:
+            self.off_proc_client.close()
+            self.serialize_proc.terminate()
+
+    def deserialize_check(self, serialized_msg, timeout=1):
+        """
+        Parameters
+        ----------
+        serialized_msg
+
+        timeout : int
+           timeout in seconds to wait for result
+        """
+        if not self.use_offprocess_checker:
+            return False
+
+        with self.serialize_lock:
+            status, info = self.off_proc_client.send_recv(serialized_msg)
+
+            if status == 'SUCCESS':
+                return True
+            if status == 'PONG':
+                return status
+            else:
+                logger.exception("Got exception while attempting deserialization")
+                raise Exception(info)
 
     def serialize(self, data):
         serialized = None
@@ -37,8 +105,9 @@ class FuncXSerializer(object):
             for method in self.methods_for_code.values():
                 try:
                     serialized = method.serialize(data)
+                    self.deserialize_check(serialized)
                 except Exception as e:
-                    logger.exception("Method {} did not work".format(method))
+                    logger.debug(f"[NON-CRITICAL] Method {method} failed with exception: {e}")
                     last_exception = e
                     continue
                 else:
