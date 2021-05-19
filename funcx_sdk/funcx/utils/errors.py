16fa2e78adc26449bc07eabfa78bf0a05f7a39b2
kyle chard <chard@uchicago.edu>
2021-05-19 15:18:34 -0500

    Updating error message

--- funcx_sdk/funcx/utils/errors.py
+++ funcx_sdk/funcx/utils/errors.py
@@ -121,4 +121,4 @@ class TaskPending(FuncxError):
         self.reason = reason
 
     def __repr__(self):
-        return "Task is pending due to {self.reason}"
+        return "Task is pending due to {}".format(self.reason)
