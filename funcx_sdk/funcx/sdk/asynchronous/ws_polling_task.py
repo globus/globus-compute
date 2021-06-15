eff891b84960943d4689593562919fad50021690
Loonride <22580625+Loonride@users.noreply.github.com>
2021-06-15 12:11:26 -0500

    add ws uri arg to test executor cli args

--- funcx_sdk/funcx/sdk/asynchronous/ws_polling_task.py
+++ funcx_sdk/funcx/sdk/asynchronous/ws_polling_task.py
@@ -20,7 +20,7 @@ class WebSocketPollingTask:
 
     def __init__(self, funcx_client,
                  loop: AbstractEventLoop,
-                 atomic_controller = None,
+                 atomic_controller=None,
                  init_task_group_id: str = None,
                  results_ws_uri: str = 'wss://api.funcx.org/ws/v2/',
                  auto_start: bool = True):
