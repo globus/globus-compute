bfc0057bdc26eb50406147a3971dc5f00ad6d2ae
Yadu Nand Babuji <yadudoc1729@gmail.com>
2021-06-28 11:31:11 -0500

    Expand members automatically

--- docs/conf.py
+++ docs/conf.py
@@ -53,6 +53,11 @@ extensions = [
 
 autosummary_generate = True
 
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True
+}
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
