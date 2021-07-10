1f07218b21a744cfec04272466b24d848ae8f41f
Zhuozhao Li <zhuozhl@clemson.edu>
2021-07-10 04:36:20 +0000

    add singularity example config to midway and theta

---
+++ docs/configs/midway_singularity.py
@@ -0,0 +1,48 @@
+from parsl.addresses import address_by_hostname
+from parsl.launchers import SrunLauncher
+from parsl.providers import SlurmProvider
+
+from funcx_endpoint.endpoint.utils.config import Config
+from funcx_endpoint.executors import HighThroughputExecutor
+
+# fmt: off
+
+# PLEASE UPDATE user_opts BEFORE USE
+user_opts = {
+    'midway': {
+        'worker_init': 'source ~/setup_funcx_test_env.sh',
+        'scheduler_options': '',
+    }
+}
+
+config = Config(
+    executors=[
+        HighThroughputExecutor(
+            max_workers_per_node=10,
+            address=address_by_hostname(),
+            scheduler_mode='soft',
+            worker_mode='singularity_reuse',
+            container_type='singularity',
+            container_cmd_options="-H /home/$USER",            
+            provider=SlurmProvider(
+                'broadwl',
+                launcher=SrunLauncher(),
+                nodes_per_block=2,
+                init_blocks=1,
+                # string to prepend to #SBATCH blocks in the submit
+                # script to the scheduler eg: '#SBATCH --constraint=knl,quad,cache'
+                scheduler_options=user_opts['midway']['scheduler_options'],
+
+                # Command to be run before starting a worker, such as:
+                # 'module load Anaconda; source activate parsl_env'.
+                worker_init=user_opts['midway']['worker_init'],
+
+                min_blocks=1,
+                max_blocks=1,
+                walltime='00:20:00'
+            ),
+        )
+    ],
+)
+
+# fmt: on
