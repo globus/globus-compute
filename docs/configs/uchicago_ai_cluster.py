e7bfac86d397091a433c18629aa4d95958fa345c
Yadu Nand Babuji <yadudoc1729@gmail.com>
2021-09-04 10:26:32 -0500

    Updating config for consistency

--- docs/configs/uchicago_ai_cluster.py
+++ docs/configs/uchicago_ai_cluster.py
@@ -1,5 +1,4 @@
 from parsl.addresses import address_by_hostname
-from parsl.channels import LocalChannel
 from parsl.launchers import SrunLauncher
 from parsl.providers import LocalProvider, SlurmProvider
 
@@ -20,26 +19,33 @@ WORKERS_PER_NODE = int(GPUS_PER_NODE / GPUS_PER_WORKER)
 GPU_MAP = ','.join([str(x) for x in range(1, TOTAL_WORKERS + 1)])
 
 config = Config(
-    executors=[HighThroughputExecutor(
-        label="fe.cs.uchicago",
-        worker_debug=False,
-        address=address_by_hostname(),
-        provider=SlurmProvider(
-            channel=LocalChannel(),
-            nodes_per_block=NODES_PER_JOB,
-            init_blocks=1,
-            partition='general',
-            # Launch 4 managers per node, each bound to 1 GPU
-            # This is a hack. We use hostname ; to terminate the srun command, and start our own
-            # DO NOT MODIFY unless you know what you are doing.
-            launcher=SrunLauncher(overrides=(f'hostname; srun --ntasks={TOTAL_WORKERS} '
-                                             f'--ntasks-per-node={WORKERS_PER_NODE} '
-                                             f'--gpus-per-task=rtx2080ti:{GPUS_PER_WORKER} '
-                                             f'--gpu-bind=map_gpu:{GPU_MAP}')
+    executors=[
+        HighThroughputExecutor(
+            label="fe.cs.uchicago",
+            worker_debug=False,
+            address=address_by_hostname(),
+            provider=SlurmProvider(
+                partition='general',
+
+                # Launch 4 managers per node, each bound to 1 GPU
+                # This is a hack. We use hostname ; to terminate the srun command, and start our own
+                # DO NOT MODIFY unless you know what you are doing.
+                launcher=SrunLauncher(overrides=(f'hostname; srun --ntasks={TOTAL_WORKERS} '
+                                                 f'--ntasks-per-node={WORKERS_PER_NODE} '
+                                                 f'--gpus-per-task=rtx2080ti:{GPUS_PER_WORKER} '
+                                                 f'--gpu-bind=map_gpu:{GPU_MAP}')
+                ),
+
+                # Scale between 0-1 blocks with 2 nodes per block
+                nodes_per_block=NODES_PER_JOB,
+                init_blocks=0,
+                min_blocks=0,
+                max_blocks=1,
+
+                # Hold blocks for 30 minutes
+                walltime='00:30:00',
             ),
-            walltime='01:00:00',
-        ),
-    )],
+        )],
 )
 
 # fmt: on
