294fca53855c12c95fbbc3ccbed5e6183658b674
Yadu Nand Babuji <yadudoc1729@gmail.com>
2021-10-25 11:53:37 -0500

    Allow funcx_service_address to be configurable from the executor
    If funcx_service_address is not specified the default address from the FuncXClient is used.

--- funcx_endpoint/funcx_endpoint/executors/high_throughput/executor.py
+++ funcx_endpoint/funcx_endpoint/executors/high_throughput/executor.py
@@ -189,6 +189,10 @@ class HighThroughputExecutor(StatusHandlingExecutor, RepresentationMixin):
         and SlurmProvider interfaces to request resources from the Slurm batch scheduler.
         Default: LocalProvider
 
+    funcx_service_address: str
+        Override funcx_service_address used by the FuncXClient. If no address is specified,
+        the FuncXClient's default funcx_service_address is used.
+        Default: None
     """
 
     def __init__(self,
@@ -230,6 +234,7 @@ class HighThroughputExecutor(StatusHandlingExecutor, RepresentationMixin):
                  managed=True,
                  interchange_local=True,
                  passthrough=True,
+                 funcx_service_address=None,
                  task_status_queue=None):
 
         logger.debug("Initializing HighThroughputExecutor")
