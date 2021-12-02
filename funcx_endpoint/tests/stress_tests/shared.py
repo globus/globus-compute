5a1db9c02016a0cc41709b1e3e761ea1e1b21626
Yadu Nand Babuji <yadudoc1729@gmail.com>
2021-12-02 16:25:07 -0600

    Moving task polling logic

--- funcx_endpoint/tests/stress_tests/shared.py
+++ funcx_endpoint/tests/stress_tests/shared.py
@@ -3,3 +3,19 @@ def simple_function(*, input_data: bytes, output_size: int, sleep_dur: int):
 
     time.sleep(sleep_dur)
     return (len(input_data), bytearray(output_size))
+
+
+def wait_for_task(fxc, task_id, walltime: int = 2):
+    import time
+
+    start = time.time()
+    while True:
+        if time.time() > start + walltime:
+            raise Exception("Timeout")
+        try:
+            r = fxc.get_result(task_id)
+        except Exception:
+            print("Not available yet")
+            time.sleep(1)
+        else:
+            return r
