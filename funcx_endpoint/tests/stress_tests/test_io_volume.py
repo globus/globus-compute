373147762c0a866c5a66a60ecb360c6790992b6a
Yadu Nand Babuji <yadudoc1729@gmail.com>
2022-01-26 19:43:31 -0600

    Adding some latency tests

--- funcx_endpoint/tests/stress_tests/test_io_volume.py
+++ funcx_endpoint/tests/stress_tests/test_io_volume.py
@@ -1,12 +1,12 @@
 import logging
-
+import time
 import pytest
 from shared import simple_function
 
 logger = logging.getLogger("test")
 
 
-# @pytest.mark.skip
+@pytest.mark.skip
 @pytest.mark.parametrize("task_count", [10000])
 def test_small_IO_through_s3_executor(fx, endpoint, task_count, result_size=100000):
     """Launches N tasks that return ~1MB results per task."""
@@ -26,6 +26,7 @@ def test_small_IO_through_s3_executor(fx, endpoint, task_count, result_size=1000
         assert x == expected_result, f"Result does not match, got: {x}"
 
 
+@pytest.mark.skip
 @pytest.mark.parametrize("task_count", [10000])
 def test_IO_through_redis_executor(fx, endpoint, task_count, result_size=10000):
     """Launches N tasks that return ~10KB results per task that would use redis"""
@@ -81,3 +82,37 @@ def test_IO_through_S3_executor(fx, endpoint, task_count, result_size=700):
             bytearray(futures[future]["output_size"]),
         )
         assert x == expected_result, f"Result does not match, got: {x}"
+
+
+# @pytest.mark.parametrize("io_size", [1, 10, 100, 1000, 10000, 100000, 1000000])
+@pytest.mark.parametrize("io_size", [1000000])
+def test_IO_latency(fx, endpoint, io_size, task_count=10):
+    """Launches N tasks that return ~1MB results per task."""
+    futures = {}
+
+
+    s_times = []
+    ttc_times = []
+    for _i in range(task_count):
+        kwargs = {"input_data": bytearray(io_size),
+                  "output_size": io_size}
+        s = time.time()
+        future = fx.submit(simple_function, sleep_dur=0, **kwargs, endpoint_id=endpoint)
+        submit = time.time() - s
+        x = future.result(timeout=60)
+        done = time.time() - s
+        logger.warning(f"Run {_i}/{task_count} done")
+        """
+        expected_result = (
+            len(futures[future]["input_data"]),
+            bytearray(futures[future]["output_size"]),
+        )
+        assert x == expected_result, f"Result does not match, got: {x}"
+        """
+        s_times.append(submit)
+        ttc_times.append(done)
+
+    avg_submit_time = sum(s_times) / len(s_times)
+    avg_ttc_time = sum(ttc_times) / len(ttc_times)
+    logger.warning(f"Submit latency over {task_count} iterations with IO of {io_size}: min={min(s_times):.3f}s max={max(s_times):.3f}s avg:{avg_submit_time:.3f}s")
+    logger.warning(f"TTC over {task_count} iterations with IO of {io_size}: min={min(ttc_times):.3f}s max={max(ttc_times):.3f}s avg:{avg_ttc_time:.3f}s")
