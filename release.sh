869e415de517f145598297103d97a2d2ee7a0976
Stephen Rosen <sirosen@globus.org>
2022-02-02 23:14:14 +0000

    Simplify release script further
    Remove unnecessary args check and helper func.

--- release.sh
+++ release.sh
@@ -11,11 +11,8 @@
 #   the version number must appear to be in use in the changelog
 #   you must have valid git config to create a signed tag (GPG key)
 #   you must have pypi credentials available to twine (e.g. ~/.pypirc)
-#
-# takes no arguments
 
 set -euo pipefail
-[ $# -eq 0 ] || exit 2
 
 VERSION="$(grep '^__version__' funcx_sdk/funcx/sdk/version.py | cut -d '"' -f 2)"
 ENDPOINT_VERSION="$(grep '^__version__' funcx_endpoint/funcx_endpoint/version.py | cut -d '"' -f 2)"
@@ -30,13 +27,12 @@ if ! grep '^funcx \& funcx\-endpoint v'"$VERSION"'$' docs/changelog.rst; then
   exit 1
 fi
 
-publish-release () {
-    pushd "$1"
-    tox -e publish-release
-    popd
-}
-
 echo "releasing v$VERSION"
 git tag -s "$VERSION" -m "v$VERSION"
-publish-release funcx_sdk
-publish-release funcx_endpoint
+
+pushd funcx_sdk
+tox -e publish-release
+popd
+
+cd funcx_endpoint
+tox -e publish-release
