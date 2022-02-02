b98f59a86fbfe41726455f8b293bed2b4c7ab40f
Yadu Nand Babuji <yadudoc1729@gmail.com>
2022-02-02 00:25:36 -0600

    Add stress tests (#676)
    * Adding stress tests
    
    * Moving task polling logic
    
    * Updating tests
    
    * Adding stress tests
    
    * Updating stress tests
    
    * Adding test that runs multiple clients in separate processes
    to mimic a multi-user env
    
    * Adding some latency tests
    
    * Unmarking skip and adding data volume to payload side
    
    * Flake8 and black fixes
    
    * Isort fixes
    
    * isort fixes only

---
+++ funcx_endpoint/tests/stress_tests/test_executor.py
@@ -0,0 +1,23 @@
+import pytest
+from shared import simple_function
+
+
+@pytest.mark.parametrize("task_count", [100, 1000])
+def test_bag_of_tasks_executor(fx, endpoint, task_count):
+    """Launches a parametrized bag of tasks using the executor interface.
+    Minimal data IO.
+    """
+    futures = []
+    for _i in range(task_count):
+        future = fx.submit(
+            simple_function,
+            input_data=bytearray(10),
+            output_size=10,
+            sleep_dur=0,
+            endpoint_id=endpoint,
+        )
+        futures.append(future)
+
+    for future in futures:
+        x = future.result()
+        assert x == (10, bytearray(10)), f"Result does not match, got: {x}"
