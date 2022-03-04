611c4173e3c5daa2542e6f21bed96d49ff5d83be
Yadu Nand Babuji <yadudoc1729@gmail.com>
2022-03-04 13:40:46 -0600

    Bumping version number to 0.3.7

--- funcx_endpoint/funcx_endpoint/version.py
+++ funcx_endpoint/funcx_endpoint/version.py
@@ -1,6 +1,6 @@
 # single source of truth for package version,
 # see https://packaging.python.org/en/latest/single_source_version/
-__version__ = "0.3.7-dev"
+__version__ = "0.3.7"
 VERSION = __version__
 
 # app name to send as part of requests
