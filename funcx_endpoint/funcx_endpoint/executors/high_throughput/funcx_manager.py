1a037db5d6c627c5a03b98bcdddf9d079df0bcbd
Ben Clifford <benc@hawaga.org.uk>
2022-03-15 16:03:55 +0100

    Apply black reformatting

--- funcx_endpoint/funcx_endpoint/executors/high_throughput/funcx_manager.py
+++ funcx_endpoint/funcx_endpoint/executors/high_throughput/funcx_manager.py
@@ -452,7 +452,9 @@ class Manager:
                                 self.outstanding_task_count
                             )
                         )
-                        log.debug(f"Task {task.task_id} pushed to task queue for type: {task_type}")
+                        log.debug(
+                            f"Task {task.task_id} pushed to task queue for type: {task_type}"
+                        )
 
             else:
                 log.debug("[TASK_PULL_THREAD] No incoming tasks")
