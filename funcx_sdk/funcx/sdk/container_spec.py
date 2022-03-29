ee2e14483859a3cf9fa7c7683b8544f99a08fb30
Ben Galewsky <ben@peartreestudio.net>
2022-03-29 14:23:08 -0500

    Add s3 url

--- funcx_sdk/funcx/sdk/container_spec.py
+++ funcx_sdk/funcx/sdk/container_spec.py
@@ -1,10 +1,13 @@
 class ContainerSpec:
-    def __init__(self, name=None, description=None, apt=None, pip=None, conda=None):
+    def __init__(self, name=None, description=None, apt=None,
+                 pip=None, conda=None,
+                 payload_url=None):
         self.name = name
         self.description = description
         self.apt = apt if apt else []
         self.pip = pip if pip else []
         self.conda = conda if conda else []
+        self.payload_url = payload_url
 
     def to_json(self):
         return {
@@ -13,4 +16,5 @@ class ContainerSpec:
             "apt": self.apt,
             "pip": self.pip,
             "conda": self.conda,
+            "payload_url": self.payload_url
         }
