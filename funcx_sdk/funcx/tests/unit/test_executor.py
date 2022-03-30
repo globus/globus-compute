d1d1e1488fd17102e4382e50aae369c3557474c7
Stephen Rosen <sirosen@globus.org>
2022-03-30 14:57:00 +0000

    Clarify what is tested in test_executor_shutdown
    Based on the original PR for this work (#736), it appeared that this
    test was checking the case in which the websocket connection exists.
    In fact, because of which part of the executor is being mocked, it is
    testing the no-connection case.
    
    This can easily be checked by trying to access the connection (via its
    property) and validating that a ValueError is raised.
    
    If the code under test changes such that the mock no longer has the
    desired effect, this will ensure it is flagged. Updated comments also
    clarify exactly what is being tested.

--- funcx_sdk/funcx/tests/unit/test_executor.py
+++ funcx_sdk/funcx/tests/unit/test_executor.py
@@ -1,12 +1,25 @@
+import pytest
+
 from funcx import FuncXExecutor
 
 
 def test_executor_shutdown(mocker):
+    """ensure that executor shutdown does not crash"""
     mocker.patch("funcx.sdk.executor.atexit")
     mocker.patch("funcx.sdk.executor.ExecutorPollerThread.event_loop_thread")
     fcli = mocker.MagicMock()
     fexe = FuncXExecutor(funcx_client=fcli)
+    # increment starts the websocket handler, decrement is a no-op
     fexe.poller_thread.atomic_controller.increment()
     fexe.poller_thread.atomic_controller.decrement()
 
-    fexe.shutdown()  # No crashing, please.
+    # there is a handler for the websocket service
+    ws_handler = fexe.poller_thread.ws_handler
+    assert ws_handler is not None
+    # but its underlying connection is not valid because the
+    # executor's main thread is mocked
+    # the connection should not be gettable: ValueError on property access
+    with pytest.raises(ValueError):
+        ws_handler.ws
+
+    fexe.shutdown()
