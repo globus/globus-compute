6fca334327534772850c2e4593bc6cf7a9bd5a0a
Ben Clifford <benc@hawaga.org.uk>
2022-03-31 15:54:51 +0200

    Move FuncXFuture earlier to make eager type evaluation happier

--- funcx_sdk/funcx/sdk/executor.py
+++ funcx_sdk/funcx/sdk/executor.py
@@ -47,6 +47,19 @@ class AtomicController:
         return f"AtomicController value:{self._value}"
 
 
+class FuncXFuture(Future):
+    """Extends concurrent.futures.Future to include an optional task UUID."""
+
+    task_id: t.Optional[str]
+    """The UUID for the task behind this Future. In batch mode, this will
+    not be populated immediately, but will appear later when the task is
+    submitted to the FuncX services."""
+
+    def __init__(self):
+        super().__init__()
+        self.task_id = None
+
+
 class FuncXExecutor(concurrent.futures.Executor):
     """Extends the concurrent.futures.Executor class to layer this interface
     over funcX. The executor returns future objects that are asynchronously
@@ -346,16 +359,3 @@ class ExecutorPollerThread:
                 ws.close(), self.eventloop
             )
             ws_close_future.result()
-
-
-class FuncXFuture(Future):
-    """Extends concurrent.futures.Future to include an optional task UUID."""
-
-    task_id: t.Optional[str]
-    """The UUID for the task behind this Future. In batch mode, this will
-    not be populated immediately, but will appear later when the task is
-    submitted to the FuncX services."""
-
-    def __init__(self):
-        super().__init__()
-        self.task_id = None
