60f6482d86b1f6a66743243333cf59cfc5d2f216
Kevin Hunter Kesling <kevin@globus.org>
2022-04-09 22:09:52 -0400

    Refactor FuncXFuture to its own file.
    Reduce size of executor, and prevent a circular import for use in
    ws_polling_task

--- funcx_sdk/funcx/sdk/executor.py
+++ funcx_sdk/funcx/sdk/executor.py
@@ -6,8 +6,8 @@ import queue
 import threading
 import time
 import typing as t
-from concurrent.futures import Future
 
+from funcx.sdk.asynchronous.funcx_future import FuncXFuture
 from funcx.sdk.asynchronous.ws_polling_task import WebSocketPollingTask
 from funcx.sdk.client import FuncXClient
 
@@ -76,19 +76,6 @@ class AtomicController:
 
     def __repr__(self):
         return f"AtomicController value:{self._value}"
-
-
-class FuncXFuture(Future):
-    """Extends concurrent.futures.Future to include an optional task UUID."""
-
-    task_id: t.Optional[str]
-    """The UUID for the task behind this Future. In batch mode, this will
-    not be populated immediately, but will appear later when the task is
-    submitted to the FuncX services."""
-
-    def __init__(self, task_id: t.Optional[str] = None):
-        super().__init__()
-        self.task_id = task_id
 
 
 class FuncXExecutor(concurrent.futures.Executor):
@@ -202,7 +189,7 @@ class FuncXExecutor(concurrent.futures.Executor):
 
         Returns
         -------
-        Future : funcx.sdk.executor.FuncXFuture
+        Future : funcx.sdk.asynchronous.funcx_future.FuncXFuture
             A future object
         """
 
