a44e73ded680e54d77aed4f19dc4c6a4832954c2
Kevin Hunter Kesling <kevin@globus.org>
2022-05-24 23:11:24 -0400

    SC-14129 Sanitize log output.
    Elide credential information from logs.  While here, write UT for
    register_endpoint to verify, and reorganize `conftest.py` in support.

--- funcx_endpoint/tests/conftest.py
+++ funcx_endpoint/tests/conftest.py
@@ -1,8 +1,17 @@
+from __future__ import annotations
+
 import os
+import random
+import string
 import uuid
 
+import globus_sdk
 import pika
 import pytest
+import responses
+
+import funcx
+from funcx.sdk.web_client import FuncxWebClient
 
 
 @pytest.fixture(scope="session")
@@ -42,3 +51,50 @@ def rabbitmq_conn_url():
 @pytest.fixture()
 def pika_conn_params(rabbitmq_conn_url):
     return pika.URLParameters(rabbitmq_conn_url)
+
+
+class FakeLoginManager:
+    def ensure_logged_in(self) -> None:
+        ...
+
+    def logout(self) -> None:
+        ...
+
+    def get_auth_client(self) -> globus_sdk.AuthClient:
+        return globus_sdk.AuthClient(authorizer=globus_sdk.NullAuthorizer())
+
+    def get_search_client(self) -> globus_sdk.SearchClient:
+        return globus_sdk.SearchClient(authorizer=globus_sdk.NullAuthorizer())
+
+    def get_funcx_web_client(self, *, base_url: str | None = None) -> FuncxWebClient:
+        return FuncxWebClient(
+            base_url="https://api2.funcx.org/v2/",
+            authorizer=globus_sdk.NullAuthorizer(),
+        )
+
+
+@pytest.fixture
+def get_standard_funcx_client():
+    responses.add(
+        method=responses.GET,
+        url="https://api2.funcx.org/v2/version",
+        headers={"Content-Type": "application/json"},
+        json={"api": "0.4.0", "min_ep_version": "0.0.0", "min_sdk_version": "0.0.0"},
+    )
+
+    def func():
+        return funcx.FuncXClient(
+            use_offprocess_checker=False,
+            login_manager=FakeLoginManager(),
+            do_version_check=False,
+        )
+
+    return func
+
+
+@pytest.fixture
+def randomstring():
+    def func(length=5, alphabet=string.ascii_letters):
+        return "".join(random.choice(alphabet) for _ in range(length))
+
+    return func
--- funcx_endpoint/tests/funcx_endpoint/conftest.py
+++ funcx_endpoint/tests/funcx_endpoint/conftest.py
@@ -1,11 +1,8 @@
 from __future__ import annotations
 
-import globus_sdk
 import pytest
 import responses
 
-import funcx
-from funcx.sdk.web_client import FuncxWebClient
 from funcx_endpoint.logging_config import add_trace_level
 
 
@@ -35,26 +32,6 @@ def _mock_logging_config(monkeypatch):
     )
 
 
-class FakeLoginManager:
-    def ensure_logged_in(self) -> None:
-        ...
-
-    def logout(self) -> None:
-        ...
-
-    def get_auth_client(self) -> globus_sdk.AuthClient:
-        return globus_sdk.AuthClient(authorizer=globus_sdk.NullAuthorizer())
-
-    def get_search_client(self) -> globus_sdk.SearchClient:
-        return globus_sdk.SearchClient(authorizer=globus_sdk.NullAuthorizer())
-
-    def get_funcx_web_client(self, *, base_url: str | None = None) -> FuncxWebClient:
-        return FuncxWebClient(
-            base_url="https://api2.funcx.org/v2/",
-            authorizer=globus_sdk.NullAuthorizer(),
-        )
-
-
 @pytest.fixture
 def setup_register_endpoint_response(endpoint_uuid):
     responses.add(
@@ -75,22 +52,3 @@ def setup_register_endpoint_response(endpoint_uuid):
             },
         },
     )
-
-
-@pytest.fixture
-def get_standard_funcx_client():
-    responses.add(
-        method=responses.GET,
-        url="https://api2.funcx.org/v2/version",
-        headers={"Content-Type": "application/json"},
-        json={"api": "0.4.0", "min_ep_version": "0.0.0", "min_sdk_version": "0.0.0"},
-    )
-
-    def func():
-        return funcx.FuncXClient(
-            use_offprocess_checker=False,
-            login_manager=FakeLoginManager(),
-            do_version_check=False,
-        )
-
-    return func
