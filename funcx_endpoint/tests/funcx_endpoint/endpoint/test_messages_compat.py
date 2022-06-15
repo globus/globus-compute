fb5d51b92b56cd0882752b56a9c7f5b60257d1dd
Chris Janidlo <chris@globus.org>
2022-06-15 14:04:58 -0500

    Switch Task to messagepack

--- funcx_endpoint/tests/funcx_endpoint/endpoint/test_messages_compat.py
+++ funcx_endpoint/tests/funcx_endpoint/endpoint/test_messages_compat.py
@@ -1,15 +1,21 @@
 import pickle
 import uuid
 
-from funcx_common.messagepack import unpack
+from funcx_common.messagepack import pack, unpack
 from funcx_common.messagepack.message_types import (
     EPStatusReport as OutgoingEPStatusReport,
 )
+from funcx_common.messagepack.message_types import Task as OutgoingTask
 
-from funcx_endpoint.endpoint.messages_compat import try_convert_to_messagepack
+from funcx_endpoint.endpoint.messages_compat import (
+    try_convert_from_messagepack,
+    try_convert_to_messagepack,
+)
 from funcx_endpoint.executors.high_throughput.messages import (
     EPStatusReport as InternalEPStatusReport,
 )
+from funcx_endpoint.executors.high_throughput.messages import Message as InternalMessage
+from funcx_endpoint.executors.high_throughput.messages import Task as InternalTask
 
 
 def test_ep_status_report_conversion():
@@ -27,3 +33,38 @@ def test_ep_status_report_conversion():
     assert external.endpoint_id == ep_id
     assert external.ep_status_report == ep_status_report
     assert external.task_statuses == task_statuses
+
+
+def test_external_task_to_internal_task():
+    task_id = uuid.uuid4()
+    container_id = uuid.uuid4()
+    task_buffer = b"task_buffer"
+
+    external = OutgoingTask(
+        task_id=task_id, container_id=container_id, task_buffer=task_buffer
+    )
+    message = pack(external)
+
+    incoming = try_convert_from_messagepack(message)
+    internal = InternalMessage.unpack(incoming)
+
+    assert isinstance(internal, InternalTask)
+    assert internal.task_id == str(task_id)
+    assert internal.container_id == str(container_id)
+    assert internal.task_buffer == task_buffer
+
+
+def test_external_task_without_container_id_converts_to_RAW():
+    task_id = uuid.uuid4()
+    task_buffer = b"task_buffer"
+
+    external = OutgoingTask(task_id=task_id, container_id=None, task_buffer=task_buffer)
+    message = pack(external)
+
+    incoming = try_convert_from_messagepack(message)
+    internal = InternalMessage.unpack(incoming)
+
+    assert isinstance(internal, InternalTask)
+    assert internal.task_id == str(task_id)
+    assert internal.container_id == "RAW"
+    assert internal.task_buffer == task_buffer
