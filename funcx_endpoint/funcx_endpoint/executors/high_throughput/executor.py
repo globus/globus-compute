7f3ab9df5aec1bc3d64f704ea265cd5f2af14cc7
Ben Clifford <benc@hawaga.org.uk>
2022-07-19 20:18:12 +0200

    Switch to recent parsl master
    This commit removes an unnecessary dependency on a
    parsl executor superclass that has been restructured
    since the previously used parsl release. It turns out
    that only one line from that superclass was needed,
    to store the specified provider, and this PR moves
    that line directly into the FuncX version of HTEX.
    
    This allows a PBS contribution to parsl from Ryan Chard
    to be used in FuncX endpoints.
    
    This should also allow more easy updating of the
    parsl dependency as/when desired.

--- funcx_endpoint/funcx_endpoint/executors/high_throughput/executor.py
+++ funcx_endpoint/funcx_endpoint/executors/high_throughput/executor.py
@@ -19,7 +19,6 @@ import daemon
 import dill
 from parsl.dataflow.error import ConfigurationError
 from parsl.executors.errors import BadMessage, ScalingFailed
-from parsl.executors.status_handling import StatusHandlingExecutor
 from parsl.providers import LocalProvider
 from parsl.utils import RepresentationMixin
 
@@ -50,7 +49,7 @@ BUFFER_THRESHOLD = 1024 * 1024
 ITEM_THRESHOLD = 1024
 
 
-class HighThroughputExecutor(StatusHandlingExecutor, RepresentationMixin):
+class HighThroughputExecutor(RepresentationMixin):
     """Executor designed for cluster-scale
 
     The HighThroughputExecutor system has the following components:
@@ -270,8 +269,7 @@ class HighThroughputExecutor(StatusHandlingExecutor, RepresentationMixin):
         task_status_queue=None,
     ):
         log.debug("Initializing HighThroughputExecutor")
-        StatusHandlingExecutor.__init__(self, provider)
-
+        self.provider = provider
         self.label = label
         self.launch_cmd = launch_cmd
         self.worker_debug = worker_debug
