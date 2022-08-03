9d5d17fac6c157d9936f366050f1ebe86cbf97d9
Kevin Hunter Kesling <kevin@globus.org>
2022-08-03 00:21:38 -0400

    No sense in iterating an empty list

--- funcx_endpoint/funcx_endpoint/endpoint/interchange.py
+++ funcx_endpoint/funcx_endpoint/endpoint/interchange.py
@@ -321,10 +321,10 @@ class EndpointInterchange:
                     len(resend_results_messages),
                 )
 
-            for results in resend_results_messages:
-                # TO-DO: Republishing backlogged/unacked messages is not supported
-                # until the types are sorted out
-                results_publisher.publish(try_convert_to_messagepack(results))
+                for results in resend_results_messages:
+                    # TO-DO: Republishing backlogged/unacked messages is not supported
+                    # until the types are sorted out
+                    results_publisher.publish(try_convert_to_messagepack(results))
 
             executor = list(self.executors.values())[0]
             last = time.time()
