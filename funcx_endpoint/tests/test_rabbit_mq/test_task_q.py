0c6cf8775d6b8fad0e0458a8ff2e9a42b045baf5
Yadu Nand Babuji <yadudoc1729@gmail.com>
2022-08-03 15:19:30 -0500

    Updates to TaskQ tests

--- funcx_endpoint/tests/test_rabbit_mq/test_task_q.py
+++ funcx_endpoint/tests/test_rabbit_mq/test_task_q.py
@@ -1,8 +1,12 @@
 import json
 import logging
 import multiprocessing
+import os
 import time
 import uuid
+
+import pytest
+from rabbitmqadmin import RabbitMQAdmin
 
 
 def test_synch(start_task_q_publisher, start_task_q_subscriber, count=10):
@@ -20,8 +24,7 @@ def test_synch(start_task_q_publisher, start_task_q_subscriber, count=10):
     logging.warning(f"Published {count} messages, closing task_q_pub")
     logging.warning("Starting task_q_subscriber")
 
-    tasks_out = multiprocessing.Queue()
-    start_task_q_subscriber(queue=tasks_out)
+    task_q, tasks_out = start_task_q_subscriber()
     for i in range(count):
         message = tasks_out.get()
         assert messages[i] == message
@@ -40,9 +43,8 @@ def test_subscriber_recovery(start_task_q_publisher, start_task_q_subscriber):
         messages.append(b_message)
 
     # Listen for 10 messages
-    tasks_out = multiprocessing.Queue()
     kill_event = multiprocessing.Event()
-    proc = start_task_q_subscriber(queue=tasks_out, kill_event=kill_event)
+    proc, tasks_out = start_task_q_subscriber(kill_event=kill_event)
     logging.warning("Proc started")
     for i in range(10):
         message = tasks_out.get()
@@ -65,7 +67,7 @@ def test_subscriber_recovery(start_task_q_publisher, start_task_q_subscriber):
 
     # Listen for the messages on a new connection
     kill_event.clear()
-    proc = start_task_q_subscriber(queue=tasks_out, kill_event=kill_event)
+    proc, tasks_out = start_task_q_subscriber(kill_event=kill_event)
 
     logging.warning("Replacement proc started")
     for i in range(10):
@@ -81,9 +83,9 @@ def test_exclusive_subscriber(start_task_q_publisher, start_task_q_subscriber):
 
     # Start two subscribers on the same rabbit queue
     tasks_out_1, tasks_out_2 = multiprocessing.Queue(), multiprocessing.Queue()
-    start_task_q_subscriber(queue=tasks_out_1)
+    _, tasks_out_1 = start_task_q_subscriber()
     time.sleep(1)
-    start_task_q_subscriber(queue=tasks_out_2)
+    _, tasks_out_2 = start_task_q_subscriber()
 
     logging.warning("TEST: Launching messages")
     # Launch 10 messages
@@ -112,8 +114,7 @@ def test_perf_combined_pub_sub_latency(start_task_q_publisher, start_task_q_subs
     """Confirm that messages published are received."""
     task_q_pub = start_task_q_publisher()
 
-    tasks_out = multiprocessing.Queue()
-    start_task_q_subscriber(queue=tasks_out)
+    _, tasks_out = start_task_q_subscriber()
 
     latency = []
     for i in range(100):
@@ -140,8 +141,8 @@ def test_perf_combined_pub_sub_throughput(
 ):
     """Confirm that messages published are received."""
     task_q_pub = start_task_q_publisher()
-    tasks_out = multiprocessing.Queue()
-    start_task_q_subscriber(queue=tasks_out)
+
+    _, tasks_out = start_task_q_subscriber()
 
     num_messages = 1000
 
@@ -171,3 +172,90 @@ def test_perf_combined_pub_sub_throughput(
         # been introduced
         assert sent_per_second > 500
         assert messages_per_second > 500
+
+
+def test_task_subscriber_stop_and_restart(
+    start_task_q_subscriber, start_task_q_publisher
+):
+    """This test confirms that messages sent are received after a stop and reset"""
+    task_q_pub = start_task_q_publisher()
+
+    for i in range(3):
+        task_q_sub, tasks_out = start_task_q_subscriber(max_reconnect_retry_count=0)
+        pid = task_q_sub.pid
+
+        b_message = f"Hello {i}".encode()
+        task_q_pub.publish(b_message)
+
+        received_message = tasks_out.get()
+        assert b_message == received_message
+
+        task_q_sub.stop()
+        # Check if process is still alive by trying to kill it
+        with pytest.raises(OSError):
+            os.kill(pid, 0)
+
+    logging.warning("All done")
+
+
+def test_task_subscriber_restart_broker_initiated(
+    start_task_q_subscriber, start_task_q_publisher, rabbitmq_conn_url
+):
+    """This test confirms that messages sent are received after a stop and reset"""
+    task_q_pub = start_task_q_publisher()
+
+    rmq_admin = RabbitMQAdmin()
+    task_q_sub, tasks_out = start_task_q_subscriber(max_reconnect_retry_count=2)
+
+    # b_message = f'Hello {i}'.encode()
+    b_message = b"Hello 1337"
+    task_q_pub.publish(b_message)
+
+    received_message = tasks_out.get()
+    assert b_message == received_message
+
+    logging.warning("Sleeping for 30 s")
+    time.sleep(30)
+    logging.warning(rmq_admin.list_connections())
+    count = rmq_admin.delete_connections()
+    assert count > 0
+    try:
+        task_q_pub.close()
+        task_q_sub.stop()
+    except Exception:
+        logging.exception("Caught exception at close")
+    logging.warning("All done")
+
+
+def test_task_subscriber_auto_reconnect(
+    start_task_q_subscriber, start_task_q_publisher
+):
+    task_q_pub = start_task_q_publisher()
+    kill_event = multiprocessing.Event()
+    rmq_admin = RabbitMQAdmin()
+    task_q_sub, tasks_out = start_task_q_subscriber(
+        kill_event=kill_event, max_reconnect_retry_count=20
+    )
+
+    b_message = b"Hello"
+    task_q_pub.publish(b_message)
+
+    received_message = tasks_out.get()
+    assert b_message == received_message
+    assert task_q_sub.connected.is_set() is True
+
+    # Delete connections
+    logging.warning(f"Connections : {rmq_admin.list_connections()}")
+    count = rmq_admin.delete_connections()
+    assert count > 0
+
+    task_q_sub.connected.wait(timeout=120)
+
+    time.sleep(50)
+    assert task_q_sub.connected.is_set() is True
+    # print("Status of task_q_sub : {}".format(task_q_sub.status))
+    # assert task_q_sub._cleanup_complete.is_set()
+
+    # Confirm that a message sent can still be received following disconnect
+    task_q_pub.publish(b_message)
+    assert tasks_out.get(timeout=10) == b_message
