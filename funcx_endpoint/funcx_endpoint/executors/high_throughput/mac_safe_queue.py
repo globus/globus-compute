cdd4217e726bb3cc764183ec01fe9af738bab6bd
Chris Janidlo <chris@globus.org>
2022-08-11 10:56:38 -0500

    Update mac_safe_queue import for new Parsl version

--- funcx_endpoint/funcx_endpoint/executors/high_throughput/mac_safe_queue.py
+++ funcx_endpoint/funcx_endpoint/executors/high_throughput/mac_safe_queue.py
@@ -1,7 +1,7 @@
 import platform
 
 if platform.system() == "Darwin":
-    from parsl.executors.high_throughput.mac_safe_queue import MacSafeQueue as mpQueue
+    from parsl.multiprocessing import MacSafeQueue as mpQueue
 else:
     from multiprocessing import Queue as mpQueue
 
