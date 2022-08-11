bdae8e8b8bf1e7de013137ac74b9d8f08db392aa
Chris Janidlo <chris@globus.org>
2022-08-11 11:14:14 -0500

    Fix mypy

--- funcx_endpoint/funcx_endpoint/executors/high_throughput/mac_safe_queue.py
+++ funcx_endpoint/funcx_endpoint/executors/high_throughput/mac_safe_queue.py
@@ -1,4 +1,8 @@
+import multiprocessing as mp
 import platform
+import typing as t
+
+mpQueue: t.Type[mp.Queue]
 
 if platform.system() == "Darwin":
     from parsl.multiprocessing import MacSafeQueue as mpQueue
