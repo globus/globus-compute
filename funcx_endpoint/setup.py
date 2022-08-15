3593745970658560e9ce40a4e09fae5bd661c61f
Yadu Nand Babuji <yadudoc1729@gmail.com>
2022-08-15 14:07:53 -0500

    Updating funcx-common 0.0.14->0.0.15

--- funcx_endpoint/setup.py
+++ funcx_endpoint/setup.py
@@ -6,7 +6,7 @@ REQUIRES = [
     "requests>=2.20.0,<3",
     "globus-sdk",  # version will be bounded by `funcx`
     "funcx==0.4.0a2",
-    "funcx-common==0.0.14",
+    "funcx-common==0.0.15",
     # table printing used in list-endpoints
     "texttable>=1.6.4,<2",
     # although psutil does not declare itself to use semver, it appears to offer
