b4ae347b757cd970b78b1591a01f0b7c22eacbcd
Kevin Hunter Kesling <kevin@globus.org>
2022-11-28 13:41:39 -0500

    Precursor refactor
    No major functional change yet, but refactoring the setup logic, and
    accordingly update the unit tests.  Biggest change related to upcoming work:
    don't create the Endpoint object until we actually need it.
    
    Other change of note: move away from deprecated `SourceFileLoader`
    
    [sc-19623]

--- funcx_endpoint/tests/unit/test_cli_behavior.py
+++ funcx_endpoint/tests/unit/test_cli_behavior.py
@@ -15,22 +15,26 @@ def funcx_dir_path(tmp_path):
 
 
 @pytest.fixture(autouse=True)
-def endpoint_obj(funcx_dir_path):
-    with mock.patch("funcx_endpoint.cli.get_cli_endpoint") as m:
+def mock_cli_state(funcx_dir_path):
+    with mock.patch("funcx_endpoint.cli.get_cli_endpoint") as mock_cli:
         mock_ep = mock.Mock()
-        mock_ep.funcx_dir = funcx_dir_path
-        mock_ep.funcx_config_file_name = "config.py"
+        mock_cli.return_value = mock_ep
+        with mock.patch("funcx_endpoint.cli.CommandState.ensure") as m_state:
+            mock_state = mock.Mock()
+            mock_state.endpoint_config_dir = funcx_dir_path
+            m_state.return_value = mock_state
 
-        m.return_value = mock_ep
-        yield mock_ep
+            yield mock_ep, mock_state
 
 
 @pytest.fixture
-def make_endpoint_dir(endpoint_obj):
+def make_endpoint_dir(mock_cli_state):
+    mock_cli, mock_state = mock_cli_state
+
     def func(name):
-        ep_dir = endpoint_obj.funcx_dir / name
+        ep_dir = mock_state.endpoint_config_dir / name
         ep_dir.mkdir(parents=True, exist_ok=True)
-        ep_config = ep_dir / endpoint_obj.funcx_config_file_name
+        ep_config = ep_dir / "config.py"
         ep_config.write_text("config = 1")  # minimal setup to make loading work
 
     return func
@@ -45,36 +49,69 @@ def cli_runner():
 def run_line(cli_runner):
     def func(argline, *, assert_exit_code: int | None = 0):
         args = shlex.split(argline) if isinstance(argline, str) else argline
+
         result = cli_runner.invoke(app, args)
         if assert_exit_code is not None:
-            assert result.exit_code == assert_exit_code
+            assert result.exit_code == assert_exit_code, (result.stdout, result.stderr)
         return result
 
     return func
 
 
-def test_start_endpoint_no_such_ep(run_line, endpoint_obj):
-    res = run_line("start foo")
-    endpoint_obj.start_endpoint.assert_not_called()
-    assert "Endpoint foo is not configured" in res.stdout
+def test_start_ep_corrupt(run_line, mock_cli_state, make_endpoint_dir):
+    make_endpoint_dir("foo")
+    mock_ep, mock_state = mock_cli_state
+    conf = mock_state.endpoint_config_dir / "foo" / "config.py"
+    conf.unlink()
+    res = run_line("start foo", assert_exit_code=1)
+    assert "corrupted?" in res.stderr
 
 
-def test_start_endpoint_existing_ep(run_line, endpoint_obj, make_endpoint_dir):
+def test_start_endpoint_no_such_ep(run_line, mock_cli_state):
+    res = run_line("start foo", assert_exit_code=1)
+    mock_ep, _ = mock_cli_state
+    mock_ep.start_endpoint.assert_not_called()
+    assert "Endpoint 'foo' is not configured" in res.stderr
+
+
+def test_start_endpoint_existing_ep(run_line, mock_cli_state, make_endpoint_dir):
     make_endpoint_dir("foo")
     run_line("start foo")
-    endpoint_obj.start_endpoint.assert_called_once()
+    mock_ep, _ = mock_cli_state
+    mock_ep.start_endpoint.assert_called_once()
 
 
-def test_stop_endpoint(run_line, endpoint_obj, make_endpoint_dir):
+def test_stop_endpoint(run_line, mock_cli_state, make_endpoint_dir):
     run_line("stop foo")
-    endpoint_obj.stop_endpoint.assert_called_once()
+    mock_ep, _ = mock_cli_state
+    mock_ep.stop_endpoint.assert_called_once()
 
 
 def test_restart_endpoint_does_start_and_stop(
-    run_line, endpoint_obj, make_endpoint_dir
+    run_line, mock_cli_state, make_endpoint_dir
 ):
     make_endpoint_dir("foo")
     run_line("restart foo")
 
-    endpoint_obj.stop_endpoint.assert_called_once()
-    endpoint_obj.start_endpoint.assert_called_once()
+    mock_ep, _ = mock_cli_state
+    mock_ep.stop_endpoint.assert_called_once()
+    mock_ep.start_endpoint.assert_called_once()
+
+
+def test_start_ep_incorrect(run_line, mock_cli_state, make_endpoint_dir):
+    make_endpoint_dir("foo")
+    mock_ep, mock_state = mock_cli_state
+    conf = mock_state.endpoint_config_dir / "foo" / "config.py"
+
+    conf.write_text("asa asd df = 5")  # fail the import
+    with mock.patch("funcx_endpoint.cli.log") as mock_log:
+        res = run_line("start foo", assert_exit_code=1)
+        assert "might be out of date" in mock_log.exception.call_args[0][0]
+    assert isinstance(res.exception, SyntaxError)
+
+    # `coverage` demands a valid syntax file.  FBOW, then, the ordering and
+    # commingling of these two tests is intentional.  Bit of a meta problem ...
+    conf.unlink()
+    conf.write_text("asdf = 5")  # syntactically correct
+    res = run_line("start foo", assert_exit_code=1)
+    assert "modified incorrectly?" in res.stderr
