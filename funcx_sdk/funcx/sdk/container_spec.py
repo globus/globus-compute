84576ab56361daa61b4c3682a9083eb946960cc7
Ben Galewsky <ben@peartreestudio.net>
2022-12-05 12:41:34 -0600

    Add type hints to container spec constructor

--- funcx_sdk/funcx/sdk/container_spec.py
+++ funcx_sdk/funcx/sdk/container_spec.py
@@ -1,13 +1,34 @@
+from __future__ import annotations
+
+
 class ContainerSpec:
     def __init__(
         self,
-        name=None,
-        description=None,
-        apt=None,
-        pip=None,
-        conda=None,
-        payload_url=None,
+        name: str | None = None,
+        description: str | None = None,
+        apt: list[str] | None = None,
+        pip: list[str] | None = None,
+        conda: list[str] | None = None,
+        payload_url: str | None = None,
     ):
+        """
+        Construct a container spec to pass to service for build operation.
+
+        Parameters
+        ----------
+        name : str
+            Name of this container to be used inside funcx
+        description : str
+            Description of the container inside funcx
+        apt : List[str]
+            List of Ubuntu library packages to install in container
+        pip : List[str]
+            List of Python libraries to install from pypi
+        conda : List[str]
+            List of Conda packages to install
+        payload_url : str
+            GitHub repo or publicly readable zip file to copy into container
+        """
         self.name = name
         self.description = description
         self.apt = apt if apt else []
