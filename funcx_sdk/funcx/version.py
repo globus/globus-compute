69be6563bb7b526e3896415a5f8ef4f9c72f659a
Chris Janidlo <chris@globus.org>
2023-01-19 13:09:25 -0600

    Bump sdk/endpoint versions and add changelog fragment

--- funcx_sdk/funcx/version.py
+++ funcx_sdk/funcx/version.py
@@ -4,7 +4,7 @@ from funcx.errors import VersionMismatch
 
 # single source of truth for package version,
 # see https://packaging.python.org/en/latest/single_source_version/
-__version__ = "1.0.7"
+__version__ = "1.0.8a"
 
 
 def compare_versions(
