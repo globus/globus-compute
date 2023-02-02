globus/globus-compute

commit fadc76b2be013668894ec9e1e925c5291efd12d7

Remove use of FuncXClient from interchanges
The last remaining use of the FuncXClient was within the *executor*
interchange, to enable looking up container location information.  The required
location information is now passed along with each task message, so modify the
logic to look there -- the key change is in `messages_compat.py`.  Everything
else is just "cleanup."

[sc-19623]

funcx_endpoint/funcx_endpoint/executors/high_throughput/interchange.py
@@ -107,7 +107,6 @@ class Interchange:
         container_cmd_options="",
         worker_mode=None,
         cold_routing_interval=10.0,
-        funcx_service_address=None,
         scaling_enabled=True,
         client_address="127.0.0.1",
         interchange_address="127.0.0.1",
@@ -120,7 +119,6 @@ class Interchange:
         logdir=".",
         endpoint_id=None,
         suppress_failure=False,
-        funcx_client=None,
     ):
         """
         Parameters
@@ -184,11 +182,6 @@ class Interchange:
         suppress_failure : Bool
              When set to True, the interchange will attempt to suppress failures.
              Default: False
-
-        funcx_client: FuncXClient
-             Only used for querying container information. If not present, container
-             info will default to RAW (ie, no container).
-             Default: None
         """
 
         self.logdir = logdir
@@ -252,8 +245,6 @@ class Interchange:
         self.pending_task_queue: dict[str, queue.Queue] = {}
         self.containers: dict[str, str] = {}
         self.total_pending_task_count = 0
-
-        self.funcx_client = funcx_client
 
         log.info(f"Interchange address is {self.interchange_address}")
         self.worker_ports = worker_ports
@@ -442,7 +433,8 @@ class Interchange:
                 log.debug("Got heartbeat")
             else:
                 log.info(f"Received task: {msg.task_id}")
-                local_container = self.get_container(msg.container_id)
+                local_container = msg.container_id
+                self.containers[local_container] = local_container
                 msg.set_local_container(local_container)
                 if local_container not in self.pending_task_queue:
                     self.pending_task_queue[local_container] = queue.Queue(
@@ -480,26 +472,6 @@ class Interchange:
                 )
                 task_counter += 1
                 log.debug(f"[TASK_PULL_THREAD] Fetched task:{task_counter}")
-
-    def get_container(self, container_uuid):
-        """Get the container image location if it is not known to the interchange"""
-        if container_uuid not in self.containers:
-            if container_uuid == "RAW" or not container_uuid:
-                self.containers[container_uuid] = "RAW"
-            else:
-                try:
-                    container = self.funcx_client.get_container(
-                        container_uuid, self.container_type
-                    )
-                except Exception:
-                    log.exception(
-                        "[FETCH_CONTAINER] Unable to resolve container location"
-                    )
-                    self.containers[container_uuid] = "RAW"
-                else:
-                    log.info(f"[FETCH_CONTAINER] Got container info: {container}")
-                    self.containers[container_uuid] = container.get("location", "RAW")
-        return self.containers[container_uuid]
 
     def get_total_tasks_outstanding(self):
         """Get the outstanding tasks in total"""
@@ -964,14 +936,14 @@ class Interchange:
                         log.debug(
                             "Received task result %s (from %s)", r["task_id"], manager
                         )
-                        task_type = self.containers[r["container_id"]]
+                        task_container = self.containers[r["container_id"]]
                         log.debug(
                             "Removing for manager: %s from %s",
                             manager,
                             self._ready_manager_queue,
                         )
 
-                        mdata["tasks"][task_type].remove(r["task_id"])
+                        mdata["tasks"][task_container].remove(r["task_id"])
 
                         # Transfer any outstanding task statuses to the result message
                         if r["task_id"] in self.task_status_deltas:
@@ -1020,18 +992,17 @@ class Interchange:
                     now,
                 )
                 log.warning(f"Too many heartbeats missed for manager {manager!r}")
-                for task_type in self._ready_manager_queue[manager]["tasks"]:
-                    for tid in self._ready_manager_queue[manager]["tasks"][task_type]:
-                        try:
-                            raise ManagerLost(manager)
-                        except Exception:
-                            result_package = {
-                                "task_id": tid,
-                                "exception": get_error_string(),
-                                "error_details": get_result_error_details(),
-                            }
-                            pkl_package = dill.dumps(result_package)
-                            bad_manager_msgs.append(pkl_package)
+                for tid in self._ready_manager_queue[manager]["tasks"].values():
+                    try:
+                        raise ManagerLost(manager)
+                    except Exception:
+                        result_package = {
+                            "task_id": tid,
+                            "exception": get_error_string(),
+                            "error_details": get_result_error_details(),
+                        }
+                        pkl_package = dill.dumps(result_package)
+                        bad_manager_msgs.append(pkl_package)
                 log.warning(f"Sent failure reports, unregistering manager {manager!r}")
                 self._ready_manager_queue.pop(manager, None)
                 if manager in interesting_managers:

funcx_endpoint/funcx_endpoint/endpoint/interchange.py
@@ -23,7 +23,6 @@ from parsl.version import VERSION as PARSL_VERSION
 
 import funcx_endpoint.endpoint.utils.config
 from funcx import __version__ as funcx_sdk_version
-from funcx.sdk.client import FuncXClient
 from funcx_endpoint import __version__ as funcx_endpoint_version
 from funcx_endpoint.endpoint.messages_compat import (
     convert_to_internaltask,
@@ -56,7 +55,6 @@ class EndpointInterchange:
         logdir=".",
         endpoint_id=None,
         endpoint_dir=".",
-        funcx_client: FuncXClient | None = None,
         result_store: ResultStore | None = None,
         reconnect_attempt_limit: int = 5,
     ):
@@ -80,9 +78,6 @@ class EndpointInterchange:
 
         endpoint_dir : pathlib.Path
              Endpoint directory path to store registration info in
-
-        funcx_client_options : Dict
-             FuncXClient initialization options
         """
         self.logdir = logdir
         log.info(
@@ -93,10 +88,6 @@ class EndpointInterchange:
         self.config = config
 
         self.endpoint_dir = endpoint_dir
-
-        if funcx_client is None:
-            funcx_client = FuncXClient()
-        self.funcx_client = funcx_client
 
         self.task_q_info = reg_info["task_queue_info"]
         self.result_q_info = reg_info["result_queue_info"]
@@ -163,10 +154,7 @@ class EndpointInterchange:
         log.info("Starting Executors")
         for executor in self.config.executors:
             if hasattr(executor, "passthrough") and executor.passthrough is True:
-                executor.start(
-                    results_passthrough=self.results_passthrough,
-                    funcx_client=self.funcx_client,
-                )
+                executor.start(results_passthrough=self.results_passthrough)
 
     def migrate_tasks_to_internal(
         self,
@@ -357,11 +345,11 @@ class EndpointInterchange:
                 # funcx-manager.  In terms of shutting down (or "rebooting") gracefully,
                 # iterate once a second whether or not a task has arrived.
                 nonlocal num_tasks_forwarded
+                ctype = executor.container_type
                 while not self._quiesce_event.is_set():
                     if self.time_to_quit:
                         self.stop()
                         continue  # nominally == break; but let event do it
-
                     try:
                         incoming_task = self.pending_task_queue.get(timeout=1)
                         task_msg = unpack(incoming_task)
@@ -376,7 +364,7 @@ class EndpointInterchange:
                         continue
 
                     try:
-                        task = convert_to_internaltask(task_msg)
+                        task = convert_to_internaltask(task_msg, ctype)
                         executor.submit_raw(task)
                         num_tasks_forwarded += 1  # Safe given GIL
 

funcx_endpoint/funcx_endpoint/endpoint/endpoint.py
@@ -325,7 +325,6 @@ class Endpoint:
                 endpoint_dir,
                 endpoint_config,
                 reg_info,
-                fx_client,
                 result_store,
             )
 
@@ -335,7 +334,6 @@ class Endpoint:
         endpoint_dir,
         endpoint_config: Config,
         reg_info,
-        funcx_client: FuncXClient,
         result_store: ResultStore,
     ):
         interchange = EndpointInterchange(
@@ -343,7 +341,6 @@ class Endpoint:
             reg_info=reg_info,
             endpoint_id=endpoint_uuid,
             endpoint_dir=endpoint_dir,
-            funcx_client=funcx_client,
             result_store=result_store,
             logdir=endpoint_dir,
         )

funcx_endpoint/funcx_endpoint/endpoint/messages_compat.py
@@ -64,10 +64,16 @@ def try_convert_to_messagepack(message: bytes) -> bytes:
     return message
 
 
-def convert_to_internaltask(message: OutgoingTask) -> bytes:
-    container_id = "RAW" if message.container_id is None else message.container_id
+def convert_to_internaltask(message: OutgoingTask, container_type: str | None) -> bytes:
+    container_loc = "RAW"
+    if message.container:
+        for img in message.container.images:
+            if img.image_type == container_type:
+                container_loc = img.location
+                break
+
     return InternalTask(
         task_id=str(message.task_id),
-        container_id=str(container_id),
+        container_id=container_loc,
         task_buffer=message.task_buffer,
     ).pack()

funcx_endpoint/funcx_endpoint/executors/high_throughput/executor.py
@@ -23,7 +23,6 @@ from parsl.executors.errors import BadMessage, ScalingFailed
 from parsl.providers import LocalProvider
 from parsl.utils import RepresentationMixin
 
-from funcx import FuncXClient
 from funcx.serialize import FuncXSerializer
 from funcx_endpoint.executors.high_throughput import interchange, zmq_pipes
 from funcx_endpoint.executors.high_throughput.mac_safe_queue import mpQueue
@@ -354,11 +353,7 @@ class HighThroughputExecutor(RepresentationMixin):
                 "--container_image={container_image} "
             )
 
-    def start(
-        self,
-        results_passthrough: multiprocessing.Queue = None,
-        funcx_client: FuncXClient = None,
-    ):
+    def start(self, results_passthrough: multiprocessing.Queue = None):
         """Create the Interchange process and connect to it."""
         self.outgoing_q = zmq_pipes.TasksOutgoing(
             "127.0.0.1", self.interchange_port_range
@@ -384,8 +379,6 @@ class HighThroughputExecutor(RepresentationMixin):
         self._executor_bad_state = threading.Event()
         self._executor_exception = None
         self._start_queue_management_thread()
-
-        self.funcx_client = funcx_client
 
         log.info("Attempting local interchange start")
         self._start_local_interchange_process()
@@ -447,7 +440,6 @@ class HighThroughputExecutor(RepresentationMixin):
                 "logdir": os.path.join(self.run_dir, self.label),
                 "suppress_failure": self.suppress_failure,
                 "endpoint_id": self.endpoint_id,
-                "funcx_client": self.funcx_client,
             },
         )
         self.queue_proc.start()

funcx_endpoint/setup.py
@@ -5,8 +5,8 @@ from setuptools import find_packages, setup
 REQUIRES = [
     "requests>=2.20.0,<3",
     "globus-sdk",  # version will be bounded by `funcx`
-    "funcx>=1.0.8a",
-    "funcx-common==0.0.23",
+    "funcx>=1.0.8",
+    "funcx-common==0.0.24",
     # table printing used in list-endpoints
     "texttable>=1.6.4,<2",
     # although psutil does not declare itself to use semver, it appears to offer

funcx_endpoint/tests/integration/funcx_endpoint/endpoint/test_interchange.py
@@ -23,9 +23,6 @@ def funcx_dir(tmp_path):
 
 
 def test_endpoint_id(mocker, funcx_dir):
-    mock_client = mocker.patch(f"{_MOCK_BASE}FuncXClient")
-    mock_client.return_value = None
-
     manager = Endpoint(funcx_dir=str(funcx_dir))
     config_dir = funcx_dir / "mock_endpoint"
 
@@ -47,11 +44,10 @@ def test_endpoint_id(mocker, funcx_dir):
         assert executor.endpoint_id == "mock_endpoint_id"
 
 
-def test_start_requires_pre_registered(mocker, funcx_dir):
+def test_start_requires_pre_registered(funcx_dir):
     with pytest.raises(TypeError):
         EndpointInterchange(
             config=Config(),
-            funcx_client=mocker.Mock(),
             reg_info=None,
             endpoint_id="mock_endpoint_id",
         )
@@ -60,7 +56,6 @@ def test_start_requires_pre_registered(mocker, funcx_dir):
 def test_invalid_message_result_returned(mocker):
     ei = EndpointInterchange(
         config=Config(executors=[mocker.Mock(endpoint_id=None)]),
-        funcx_client=mocker.Mock(),
         reg_info={"task_queue_info": {}, "result_queue_info": {}},
     )
 

funcx_endpoint/tests/integration/funcx_endpoint/endpoint/test_interchange_with_rabbit.py
@@ -4,7 +4,6 @@ import multiprocessing
 import time
 import uuid
 import warnings
-from unittest import mock
 
 import dill
 import pika
@@ -29,9 +28,7 @@ def run_interchange_process(
     a random endpoint id, and the mocked registration info.
     """
 
-    def run_it(reg_info: dict, endpoint_uuid, endpoint_dir, mock_funcx_cli=True):
-        fake_client = mock.Mock() if mock_funcx_cli else None
-
+    def run_it(reg_info: dict, endpoint_uuid, endpoint_dir):
         mock_exe = MockExecutor()
         mock_exe.endpoint_id = endpoint_uuid
 
@@ -41,7 +38,6 @@ def run_interchange_process(
             config=config,
             endpoint_id=endpoint_uuid,
             reg_info=reg_info,
-            funcx_client=fake_client,
             endpoint_dir=endpoint_dir,
         )
 

commit cb5af4db6ca7681aa459969cb05e3adf1c808ce5

Correct data structure traversal
Broken in `fadc76b2be`; I missed the nested structure.

funcx_endpoint/funcx_endpoint/executors/high_throughput/interchange.py
@@ -992,22 +992,24 @@ class Interchange:
                     now,
                 )
                 log.warning(f"Too many heartbeats missed for manager {manager!r}")
-                for tid in self._ready_manager_queue[manager]["tasks"].values():
-                    try:
-                        raise ManagerLost(manager)
-                    except Exception:
-                        result_package = {
-                            "task_id": tid,
-                            "exception": get_error_string(),
-                            "error_details": get_result_error_details(),
-                        }
-                        pkl_package = dill.dumps(result_package)
-                        bad_manager_msgs.append(pkl_package)
-                log.warning(f"Sent failure reports, unregistering manager {manager!r}")
+                for tasks in self._ready_manager_queue[manager]["tasks"].values():
+                    for tid in tasks:
+                        try:
+                            raise ManagerLost(manager)
+                        except Exception:
+                            result_package = {
+                                "task_id": tid,
+                                "exception": get_error_string(),
+                                "error_details": get_result_error_details(),
+                            }
+                            pkl_package = dill.dumps(result_package)
+                            bad_manager_msgs.append(pkl_package)
+                log.warning(f"Unregistering manager {manager!r}")
                 self._ready_manager_queue.pop(manager, None)
                 if manager in interesting_managers:
                     interesting_managers.remove(manager)
             if bad_manager_msgs:
+                log.warning(f"Sending task failure reports of manager {manager!r}")
                 self.results_outgoing.send(dill.dumps(bad_manager_msgs))
             log.trace("ending one main loop iteration")
 