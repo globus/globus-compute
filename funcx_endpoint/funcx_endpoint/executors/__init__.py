c800d94c8d35da4a3e9a321d47bb8fdd6df0c388
Yadu Babuji <yadudoc1729@gmail.com>
2023-02-20 11:02:52 -0600

    Updating module init files

--- funcx_endpoint/funcx_endpoint/executors/__init__.py
+++ funcx_endpoint/funcx_endpoint/executors/__init__.py
@@ -1,3 +1,5 @@
+from funcx_endpoint.executors.gc_executor.gc_executor import GCExecutor
 from funcx_endpoint.executors.high_throughput.executor import HighThroughputExecutor
+from funcx_endpoint.executors.process_pool.process_pool import ProcessPoolExecutor
 
-__all__ = ["HighThroughputExecutor"]
+__all__ = ["HighThroughputExecutor", "ProcessPoolExecutor", "GCExecutor"]
--- funcx_endpoint/funcx_endpoint/executors/parsl_htex/__init__.py
+++
@@ -1,3 +0,0 @@
-from funcx_endpoint.executors.parsl_htex.parsl_htex import ParslHTEX
-
-__all__ = ["ParslHTEX"]
