17d517eee92d1477fefc6ee95f3e640090efde19
Kevin Hunter Kesling <kevin@globus.org>
2023-04-13 12:15:41 -0400

    Undo rebrand-lost change
    The rebrand (9706b5f5ca2e84581b007c6c38036709eb674b06), mistakenly put back the
    *old* line.  Undo the undo!

--- helm/boot.sh
+++ helm/boot.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+set -e
+
 EP_NAME="$1"; shift
 EP_UUID="$1"; shift
 
 echo -e "\n  Preparing to start kubelet Endpoint: $EP_UUID ($EP_NAME)\n"
 
 mkdir -p "$HOME/.globus_compute/$EP_NAME/"
-cp /compute/"$EP_NAME"/* "$HOME/.globus_compute/$EP_NAME/"
+cp /compute/ep_instance/* "$HOME/.globus_compute/$EP_NAME/"
 cp /compute/config/config.py "$HOME/.globus_compute/"
 
 if [[ -e "/compute/credentials/storage.db" ]]; then
