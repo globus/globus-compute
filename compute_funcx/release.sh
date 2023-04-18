915fd2349770ee90c843a0012efbf962036b6a30
Lei Wang <lei@globus.org>
2023-04-18 14:01:56 -0500

    remove changelog check for now
    Revert "remove changelog check for now"
    
    This reverts commit 9ba916e21a4cd1a97819cb4205d284c90e5055a9.
    
    add changelog to funcx wrapper

--- compute_funcx/release.sh
+++ compute_funcx/release.sh
@@ -21,12 +21,25 @@ if [[ "$VERSION" != "$ENDPOINT_VERSION" ]]; then
   exit 1
 fi
 
+if ! grep '^funcx \& funcx\-endpoint v'"$VERSION"'$' changelog.rst; then
+  echo "package version v$VERSION not noted in changelog.rst"
+  exit 1
+fi
+
 echo "releasing v$VERSION"
-git tag -s "$VERSION" -m "v$VERSION"
+if [ git tag -s "$VERSION" -m "v$VERSION" ]; then
+  echo "Git tagged $VERSION"
+else
+  read -p "Tag $VERSION already exists.  Release packages with this VERSION anyway? [y/n] " -n 1 -r
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+  fi
+fi
 
 pushd sdk
 tox -e publish-release
 popd
 
-cd endpoint
+pushd endpoint
 tox -e publish-release
+popd
