8cbed3718302bd5bdc7133aa44f7045a677061b7
LeiGlobus <101125281+LeiGlobus@users.noreply.github.com>
2023-05-03 16:19:08 -0400

    bump versions for alpha release (#1139)

--- changelog.d/20230421_111659_kevin_address_concurrent_data_structure_modification.rst
+++
@@ -1,5 +0,0 @@
-Bug Fixes
-^^^^^^^^^
-
-- Address a concurrent data structure modification error that resulted in
-  stalled processing and lost tasks
--- compute_funcx/sdk/setup.py
+++ compute_funcx/sdk/setup.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from setuptools import find_packages, setup
 
 REQUIRES = [
-    "globus-compute-sdk>=2.0.1",
+    "globus-compute-sdk>=2.0.2a0",
 ]
 
 
