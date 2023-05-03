ericblau/funcX

Support user-definable compute dir via env var

compute_endpoint/globus_compute_endpoint/cli.py
@@ -41,7 +41,7 @@ class CommandState:
 def init_config_dir() -> pathlib.Path:
     try:
         return ensure_compute_dir()
-    except FileExistsError as e:
+    except (FileExistsError, FileNotFoundError) as e:
         raise ClickException(str(e))
 
 

globus/globus-compute

compute_endpoint/globus_compute_endpoint/cli.py
@@ -41,7 +41,7 @@ class CommandState:
 def init_config_dir() -> pathlib.Path:
     try:
         return ensure_compute_dir()
-    except FileExistsError as e:
+    except (FileExistsError, PermissionError) as e:
         raise ClickException(str(e))
 
 