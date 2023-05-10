globus/globus-compute

commit 273e39002cf1ceda613ba6ad7893b87549c22046

Teach interchange to send bite-sized updates
Previously, the endpoint could send an arbitrarily large status report packet.
While the services handle the packet just fine (regardless of size), it's just
generally courteous to chunk large amounts work.  So, play by the "unwritten
rules of courtesy."

[sc-19723]

compute_endpoint/globus_compute_endpoint/executors/high_throughput/interchange.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import argparse
 import collections
+import copy
 import json
 import logging
 import os
@@ -34,6 +35,7 @@ from globus_compute_endpoint.executors.high_throughput.messages import (
     MessageType,
 )
 from globus_compute_endpoint.logging_config import ComputeLogger
+from globus_compute_sdk.sdk.utils import chunk_by
 from globus_compute_sdk.serialize import ComputeSerializer
 from parsl.version import VERSION as PARSL_VERSION
 
@@ -551,20 +553,33 @@ class Interchange:
             self._ready_manager_queue[manager]["active"] = False
 
     def _status_report_loop(self, kill_event, status_report_queue: queue.Queue):
-        log.debug("Status reporting loop starting")
         log.info(f"Endpoint id: {self.endpoint_id}")
 
         while True:
-            log.trace("Endpoint id : %s, %s", self.endpoint_id, type(self.endpoint_id))
             with self._task_status_delta_lock:
-                msg = EPStatusReport(
-                    self.endpoint_id,
-                    self.get_global_state_for_status_report(),
-                    self.task_status_deltas,
-                )
-                status_report_queue.put(msg.pack())
+                task_status_deltas = copy.deepcopy(self.task_status_deltas)
                 self.task_status_deltas.clear()
-            log.debug("Sending status report to executor, and clearing task deltas.")
+
+            log.debug(
+                "Cleared task deltas (%s); sending status report to executor.",
+                len(task_status_deltas),
+            )
+
+            # The result processor will gracefully handle any size message, but
+            # courtesy says to chunk work; 4,096 is empirically chosen to be plenty
+            # "bulk enough," but not rude.
+            for tsd_chunk in chunk_by(task_status_deltas.items(), 4_096):
+                try:
+                    msg = EPStatusReport(
+                        self.endpoint_id,
+                        self.get_global_state_for_status_report(),
+                        dict(tsd_chunk),
+                    )
+                    status_report_queue.put(msg.pack())
+                except Exception:
+                    log.exception("Unable to create or send EP status report.")
+                    log.debug("Attempted to send chunk: %s", tsd_chunk)
+                    # ignoring so that the thread continues; "it's just a status"
 
             if kill_event.wait(self.heartbeat_period):
                 break

commit fbd14d7df143e6d7f48c628d8a5c531a3152f259

Send EPStatusReport even if no outstanding tasks
The interchange should send an EPStatusReport every `heartbeat_period`, period.
273e39002cf1ceda613ba6ad7893b87549c22046 broke that expectation for the case of
"no task deltas" -- a common occurrence for the tests we run because when a
task is done, it's delta is handled directly by the task-specific result
message and removed from the outstanding "task status deltas" data structure.

Even more than tests, however, it's also a problem when there are actually no
tasks running -- when the endpoint is legitimately idle, it should still be
marked as "online."

Follow up for [sc-19273]

compute_endpoint/globus_compute_endpoint/executors/high_throughput/interchange.py
@@ -555,6 +555,15 @@ class Interchange:
     def _status_report_loop(self, kill_event, status_report_queue: queue.Queue):
         log.info(f"Endpoint id: {self.endpoint_id}")
 
+        def _enqueue_status_report(ep_state: dict, task_states: dict):
+            try:
+                msg = EPStatusReport(self.endpoint_id, ep_state, dict(task_states))
+                status_report_queue.put(msg.pack())
+            except Exception:
+                log.exception("Unable to create or send EP status report.")
+                log.debug("Attempted to send chunk: %s", tsd_chunk)
+                # ignoring so that the thread continues; "it's just a status"
+
         while True:
             with self._task_status_delta_lock:
                 task_status_deltas = copy.deepcopy(self.task_status_deltas)
@@ -565,21 +574,20 @@ class Interchange:
                 len(task_status_deltas),
             )
 
+            # For multi-chunk reports ("lots-o-tasks"), the state won't change *that*
+            # much each iteration, so cache the result
+            global_state = self.get_global_state_for_status_report()
+
             # The result processor will gracefully handle any size message, but
             # courtesy says to chunk work; 4,096 is empirically chosen to be plenty
             # "bulk enough," but not rude.
             for tsd_chunk in chunk_by(task_status_deltas.items(), 4_096):
-                try:
-                    msg = EPStatusReport(
-                        self.endpoint_id,
-                        self.get_global_state_for_status_report(),
-                        dict(tsd_chunk),
-                    )
-                    status_report_queue.put(msg.pack())
-                except Exception:
-                    log.exception("Unable to create or send EP status report.")
-                    log.debug("Attempted to send chunk: %s", tsd_chunk)
-                    # ignoring so that the thread continues; "it's just a status"
+                _enqueue_status_report(global_state, dict(tsd_chunk))
+
+            if not task_status_deltas:
+                _enqueue_status_report(global_state, {})
+
+            del task_status_deltas  # free some memory in "large case"
 
             if kill_event.wait(self.heartbeat_period):
                 break