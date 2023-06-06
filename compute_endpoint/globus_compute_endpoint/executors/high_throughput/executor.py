2c9f365a41162db06a7bde918ac79843c857a9e5
Yadu Babuji <yadudoc1729@gmail.com>
2023-06-06 11:08:10 -0500

    * Updating tests for GC_TASK_TIMEOUT and MaxResultSizeExceeded
    * Updating HTEX to a factory that makes HTEngine and throws a warning note
    * Adding tests
    
    * Adding typing for HTEXFuture and `__slots__`
    * Renaming executor->engine

--- compute_endpoint/globus_compute_endpoint/executors/high_throughput/executor.py
+++ compute_endpoint/globus_compute_endpoint/executors/high_throughput/executor.py
@@ -1,3 +1,21 @@
+import logging
+
 from globus_compute_endpoint.engines import HighThroughputEngine
 
-HighThroughputExecutor = HighThroughputEngine
+log = logging.getLogger(__name__)
+
+warning_message = (
+    "WARNING: \n"
+    "The HighThroughputExecutor will be deprecated by globus-compute-endpoint v2.2 "
+    "and will be replaced by the HighThroughputEngine"
+)
+
+
+class HighThroughputExecutor:
+    """The HighThroughputExecutor class is a wrapper
+    over the engines.HighThroughputEngine
+    """
+
+    def __new__(cls, *args, **kwargs):
+        print(warning_message)
+        return object.__new__(HighThroughputEngine, *args, **kwargs)
