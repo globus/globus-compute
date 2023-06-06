032704088546f5e480f59c1efda917cc8f234538
Yadu Babuji <yadudoc1729@gmail.com>
2023-06-06 14:27:40 -0500

    * Cleaner deprecation warning and tests

--- compute_endpoint/tests/unit/test_htex_facade.py
+++ compute_endpoint/tests/unit/test_htex_facade.py
@@ -2,13 +2,17 @@ from globus_compute_endpoint.engines import HighThroughputEngine
 from globus_compute_endpoint.executors import HighThroughputExecutor
 
 
-def test_deprecation_notice(capfd):
+def test_deprecation_notice(mocker):
     """Instantiating HTEX should throw a WARNING notice about
     deprecation
     """
+    mock_warn = mocker.patch(
+        "globus_compute_endpoint.executors.high_throughput.executor.warnings"
+    )
     HighThroughputExecutor()
-    out, err = capfd.readouterr()
-    assert "WARNING" in out
+    assert mock_warn.warn.called
+    # call_args is weirdly nested
+    assert "deprecated" in mock_warn.warn.call_args[0][0]
 
 
 def test_htex_retuns_engine():
