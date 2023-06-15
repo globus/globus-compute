f7c74bf21e60bf37de3e17cb5b9441fb7fccfd47
Kevin Hunter Kesling <kevin@globus.org>
2023-06-15 10:15:57 -0400

    Remove processpool
    We'll rebase and squash to remove this commit entirely from the branch.  But
    for now, while the two of us work together ...

--- compute_endpoint/tests/unit/test_status_reporting.py
+++ compute_endpoint/tests/unit/test_status_reporting.py
@@ -1,62 +1,41 @@
 import logging
 import multiprocessing
-import os
-import shutil
 import uuid
 
 import pytest
 from globus_compute_common import messagepack
 from globus_compute_common.messagepack.message_types import EPStatusReport
-from globus_compute_endpoint.engines import GlobusComputeEngine, ProcessPoolEngine
-from pytest import fixture
+from globus_compute_endpoint import engines
 
 logger = logging.getLogger(__name__)
 
 HEARTBEAT_PERIOD = 0.1
 
 
-@fixture
-def proc_pool_engine():
+@pytest.fixture
+def gc_engine(tmp_path):
     ep_id = uuid.uuid4()
-    executor = ProcessPoolEngine(max_workers=2, heartbeat_period_s=HEARTBEAT_PERIOD)
-    queue = multiprocessing.Queue()
-    executor.start(endpoint_id=ep_id, run_dir="/tmp", results_passthrough=queue)
-
-    yield executor
-    executor.shutdown()
-
-
-@fixture
-def gc_engine():
-    ep_id = uuid.uuid4()
-    executor = GlobusComputeEngine(
+    engine = engines.GlobusComputeEngine(
         address="127.0.0.1",
         heartbeat_period_s=HEARTBEAT_PERIOD,
         heartbeat_threshold=1,
     )
     queue = multiprocessing.Queue()
-    tempdir = "/tmp/HTEX_logs"
-    os.makedirs(tempdir, exist_ok=True)
-    executor.start(endpoint_id=ep_id, run_dir=tempdir, results_passthrough=queue)
+    engine.start(endpoint_id=ep_id, run_dir=str(tmp_path), results_passthrough=queue)
 
-    yield executor
-    executor.shutdown()
-    shutil.rmtree(tempdir, ignore_errors=True)
+    yield engine
+    engine.shutdown()
 
 
-@pytest.mark.parametrize("x", ["gc_engine", "proc_pool_engine"])
-def test_status_reporting(x, gc_engine, proc_pool_engine):
-    if x == "gc_engine":
-        executor = gc_engine
-    elif x == "proc_pool_engine":
-        executor = proc_pool_engine
+def test_status_reporting(x, gc_engine):
+    engine = gc_engine
 
-    report = executor.get_status_report()
+    report = engine.get_status_report()
     assert isinstance(report, EPStatusReport)
 
-    results_q = executor.results_passthrough
+    results_q = engine.results_passthrough
 
-    assert executor._status_report_thread.reporting_period == HEARTBEAT_PERIOD
+    assert engine._status_report_thread.reporting_period == HEARTBEAT_PERIOD
 
     # Flush queue to start
     while not results_q.empty():
