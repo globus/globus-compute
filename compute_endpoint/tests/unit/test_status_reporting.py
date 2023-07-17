06cff880fd675682cb9ce36b74d9e51d27efb008
Reid Mello <30907815+rjmello@users.noreply.github.com>
2023-07-17 11:36:00 -0400

    Consolidate engine pytest fixtures

--- compute_endpoint/tests/unit/test_status_reporting.py
+++ compute_endpoint/tests/unit/test_status_reporting.py
@@ -1,7 +1,4 @@
 import logging
-import multiprocessing
-import uuid
-from queue import Queue
 
 import pytest
 from globus_compute_common import messagepack
@@ -10,56 +7,13 @@ from globus_compute_endpoint import engines
 
 logger = logging.getLogger(__name__)
 
-HEARTBEAT_PERIOD = 0.1
-
-
-@pytest.fixture
-def proc_pool_engine(tmp_path):
-    ep_id = uuid.uuid4()
-    engine = engines.ProcessPoolEngine(
-        max_workers=2, heartbeat_period_s=HEARTBEAT_PERIOD
-    )
-    queue = multiprocessing.Queue()
-    engine.start(endpoint_id=ep_id, run_dir=str(tmp_path), results_passthrough=queue)
-
-    yield engine
-    engine.shutdown()
-
-
-@pytest.fixture
-def thread_pool_engine(tmp_path):
-    ep_id = uuid.uuid4()
-    engine = engines.ThreadPoolEngine(
-        max_workers=2, heartbeat_period_s=HEARTBEAT_PERIOD
-    )
-
-    queue = Queue()
-    engine.start(endpoint_id=ep_id, run_dir=str(tmp_path), results_passthrough=queue)
-
-    yield engine
-    engine.shutdown()
-
-
-@pytest.fixture
-def gc_engine(tmp_path):
-    ep_id = uuid.uuid4()
-    engine = engines.GlobusComputeEngine(
-        address="127.0.0.1",
-        heartbeat_period_s=HEARTBEAT_PERIOD,
-        heartbeat_threshold=1,
-    )
-    queue = multiprocessing.Queue()
-    engine.start(endpoint_id=ep_id, run_dir=str(tmp_path), results_passthrough=queue)
-
-    yield engine
-    engine.shutdown()
-
 
 @pytest.mark.parametrize("engine_type", ["proc_pool", "thread_pool", "gc"])
 def test_status_reporting(
     proc_pool_engine: engines.ProcessPoolEngine,
     thread_pool_engine: engines.ThreadPoolEngine,
     gc_engine: engines.GlobusComputeEngine,
+    engine_heartbeat: float,
     engine_type: str,
 ):
     if engine_type == "proc_pool":
@@ -74,7 +28,7 @@ def test_status_reporting(
 
     results_q = engine.results_passthrough
 
-    assert engine._status_report_thread.reporting_period == HEARTBEAT_PERIOD
+    assert engine._status_report_thread.reporting_period == engine_heartbeat
 
     # Flush queue to start
     while not results_q.empty():
