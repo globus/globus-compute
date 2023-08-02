7fb7fdaedac39b9255cfc3356ac40db072f3ad05
Reid Mello <30907815+rjmello@users.noreply.github.com>
2023-08-02 15:01:57 -0400

    Bump versions for v2.3.0a0 release

--- compute_funcx/endpoint/setup.py
+++ compute_funcx/endpoint/setup.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from setuptools import find_packages, setup
 
 REQUIRES = [
-    "globus-compute-endpoint==2.2.4",
+    "globus-compute-endpoint==2.3.0a0",
 ]
 
 version_ns = {}
