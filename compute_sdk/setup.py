2b10d3c6a8f91763591c2c296e1f5a79573bda5e
Lei Wang <lei@globus.org>
2023-08-03 12:18:39 -0400

    Revert dill version to make it compatible with 2.2.3

--- compute_sdk/setup.py
+++ compute_sdk/setup.py
@@ -14,7 +14,7 @@ REQUIRES = [
     # dill is an extension of `pickle` to a wider array of native python types
     # pin to the latest version, as 'dill' is not at 1.0 and does not have a clear
     # versioning and compatibility policy
-    "dill==0.3.6",
+    "dill==0.3.5.1",
     # typing_extensions, so we can use Protocol and other typing features on python3.7
     'typing_extensions>=4.0;python_version<"3.8"',
     # packaging, allowing version parsing
