3d05f58b0825564c54ae5335754196311da9e1dc
Kevin Hunter Kesling <kevin@globus.org>
2023-08-11 16:23:54 -0400

    Prevent same-user child-processes (#1266)
    For administrators running a multi-tenant endpoint, the parent process -- the
    endpoint manager -- is expected to lookup local user ids (UID) and then drop
    privileges to that UID before `exec()`ing.  On the other hand, for non-power
    users (e.g., not `root`) running the multi-tenant endpoint, the process won't
    have the privileges to change users anyway.  Consequently, implement a check
    that only allows the parent endpoint process to create child endpoint processes
    ("user endpoints") with the same process UID if the UID is not otherwise
    privileged.
    
    This is implemented with 2 checks:
    
    - Is the parent process owned by UID 0?
    - Does the parent process have any admin-like capabilities (via `pyprctl`)
    
    If either are true, then set a flag to prevent any attempts to create a child
    process with the same UID.
    
    If neither are true -- so, when a "regular user" is running the multitenant
    endpoint manager -- then only proceed if the MT UID and the local user UID
    _do_, in fact, match -- don't even try to execute the `os.set*id()` syscalls.
    
    [sc-26381]

--- compute_endpoint/globus_compute_endpoint/endpoint/utils/__init__.py
+++ compute_endpoint/globus_compute_endpoint/endpoint/utils/__init__.py
@@ -1,11 +1,56 @@
+import os as _os
+import pwd as _pwd
 import re as _re
 import typing as t
+
+import pyprctl as _pyprctl
 
 _url_user_pass_pattern = r"://([^:]+):[^@]+@"
 _url_user_pass_re = _re.compile(_url_user_pass_pattern)
 _urlb_user_pass_re = _re.compile(_url_user_pass_pattern.encode())
 
 _T = t.TypeVar("_T", str, bytes)
+
+# List of Linux capabilities that would suggest the running process is
+# privileged, even if os.getuid() != 0.
+_MULTI_USER_CAPS = {
+    _pyprctl.Cap.AUDIT_CONTROL,
+    _pyprctl.Cap.AUDIT_READ,
+    _pyprctl.Cap.BPF,
+    _pyprctl.Cap.CHECKPOINT_RESTORE,
+    _pyprctl.Cap.CHOWN,
+    _pyprctl.Cap.DAC_OVERRIDE,
+    _pyprctl.Cap.DAC_READ_SEARCH,
+    _pyprctl.Cap.FOWNER,
+    _pyprctl.Cap.FSETID,
+    _pyprctl.Cap.IPC_OWNER,
+    _pyprctl.Cap.KILL,
+    _pyprctl.Cap.LINUX_IMMUTABLE,
+    _pyprctl.Cap.MAC_ADMIN,
+    _pyprctl.Cap.MAC_OVERRIDE,
+    _pyprctl.Cap.MKNOD,
+    _pyprctl.Cap.NET_ADMIN,
+    _pyprctl.Cap.NET_BIND_SERVICE,
+    _pyprctl.Cap.NET_RAW,
+    _pyprctl.Cap.PERFMON,
+    _pyprctl.Cap.SETGID,
+    _pyprctl.Cap.SETFCAP,
+    _pyprctl.Cap.SETPCAP,
+    _pyprctl.Cap.SETUID,
+    _pyprctl.Cap.SYS_ADMIN,
+    _pyprctl.Cap.SYS_BOOT,
+    _pyprctl.Cap.SYS_CHROOT,
+    _pyprctl.Cap.SYS_MODULE,
+    _pyprctl.Cap.SYS_NICE,
+    _pyprctl.Cap.SYS_PACCT,
+    _pyprctl.Cap.SYS_PTRACE,
+    _pyprctl.Cap.SYS_RAWIO,
+    _pyprctl.Cap.SYS_RESOURCE,
+    _pyprctl.Cap.SYS_TIME,
+    _pyprctl.Cap.SYS_TTY_CONFIG,
+    _pyprctl.Cap.SYSLOG,
+    _pyprctl.Cap.WAKE_ALARM,
+}
 
 
 def _redact_url_creds(raw: _T, redact_user=True, repl="***", count=0) -> _T:
@@ -27,3 +72,14 @@ def _redact_url_creds(raw: _T, redact_user=True, repl="***", count=0) -> _T:
     if isinstance(raw, str):
         return _url_user_pass_re.sub(repl=repl, string=raw, count=count)
     return _urlb_user_pass_re.sub(repl=repl.encode(), string=raw, count=count)
+
+
+def is_privileged(posix_user=None):
+    if not posix_user:
+        posix_user = _pwd.getpwuid(_os.getuid())
+
+    proc_caps = _pyprctl.CapState.get_current()
+    has_privileges = posix_user.pw_uid == 0
+    has_privileges |= posix_user.pw_name == "root"
+    has_privileges |= any(c in proc_caps.effective for c in _MULTI_USER_CAPS)
+    return has_privileges
