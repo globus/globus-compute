kurtmckee/pr-funcX

Support Globus auth policies on endpoints
Endpoint admins can now define a Globus authentication policy to
evaluate endpoint users against. The web service handles both evaluation
and enforcement.

compute_endpoint/globus_compute_endpoint/cli.py
@@ -234,6 +234,10 @@ def version_command():
     "--display-name",
     help="A human readable display name for the endpoint, if desired",
 )
+@click.option(
+    "--auth-policy",
+    help="Endpoint users are evaluated against this Globus authentication policy",
+)
 @name_arg
 @common_options
 def configure_endpoint(
@@ -242,6 +246,7 @@ def configure_endpoint(
     endpoint_config: str | None,
     multi_tenant: bool,
     display_name: str | None,
+    auth_policy: str | None,
 ):
     """Configure an endpoint
 
@@ -258,7 +263,9 @@ def configure_endpoint(
 
     compute_dir = get_config_dir()
     ep_dir = compute_dir / name
-    Endpoint.configure_endpoint(ep_dir, endpoint_config, multi_tenant, display_name)
+    Endpoint.configure_endpoint(
+        ep_dir, endpoint_config, multi_tenant, display_name, auth_policy
+    )
 
 
 @app.command(name="start", help="Start an endpoint")

compute_endpoint/globus_compute_endpoint/endpoint/endpoint.py
@@ -71,12 +71,16 @@ class Endpoint:
         target_path: pathlib.Path,
         multi_tenant: bool,
         display_name: str | None,
+        auth_policy: str | None,
     ):
         config_text = original_path.read_text()
         config_dict = yaml.safe_load(config_text)
 
         if display_name:
             config_dict["display_name"] = display_name
+
+        if auth_policy:
+            config_dict["authentication_policy"] = auth_policy
 
         if multi_tenant:
             config_dict["multi_tenant"] = multi_tenant
@@ -90,6 +94,7 @@ class Endpoint:
         endpoint_config: pathlib.Path | None = None,
         multi_tenant=False,
         display_name: str | None = None,
+        auth_policy: str | None = None,
     ):
         """Initialize a clean endpoint dir
 
@@ -98,6 +103,7 @@ class Endpoint:
             the Globus Compute default config file
         :param multi_tenant: Whether the endpoint is a multi-user endpoint
         :param display_name: A display name to use, if desired
+        :param auth_policy: Globus authentication policy
         """
         log.debug(f"Creating endpoint dir {endpoint_dir}")
         user_umask = os.umask(0o0077)
@@ -120,6 +126,7 @@ class Endpoint:
                 config_target_path,
                 multi_tenant,
                 display_name,
+                auth_policy,
             )
 
             if multi_tenant:
@@ -153,6 +160,7 @@ class Endpoint:
         endpoint_config: str | None,
         multi_tenant: bool = False,
         display_name: str | None = None,
+        auth_policy: str | None = None,
     ):
         ep_name = conf_dir.name
         if conf_dir.exists():
@@ -162,7 +170,7 @@ class Endpoint:
 
         templ_conf_path = pathlib.Path(endpoint_config) if endpoint_config else None
         Endpoint.init_endpoint_dir(
-            conf_dir, templ_conf_path, multi_tenant, display_name
+            conf_dir, templ_conf_path, multi_tenant, display_name, auth_policy
         )
         config_path = Endpoint._config_file_path(conf_dir)
         if multi_tenant:
@@ -378,6 +386,7 @@ class Endpoint:
                     multi_tenant=False,
                     display_name=endpoint_config.display_name,
                     allowed_functions=endpoint_config.allowed_functions,
+                    auth_policy=endpoint_config.authentication_policy,
                 )
 
             except GlobusAPIError as e:

globus/globus-compute

compute_endpoint/globus_compute_endpoint/cli.py
@@ -234,6 +234,14 @@ def version_command():
     "--display-name",
     help="A human readable display name for the endpoint, if desired",
 )
+@click.option(
+    "--auth-policy",
+    help=(
+        "Endpoint users are evaluated against this Globus authentication policy. "
+        "For more information on Globus authentication policies, visit "
+        "https://docs.globus.org/api/auth/developer-guide/#authentication-policies."
+    ),
+)
 @name_arg
 @common_options
 def configure_endpoint(
@@ -242,6 +250,7 @@ def configure_endpoint(
     endpoint_config: str | None,
     multi_user: bool,
     display_name: str | None,
+    auth_policy: str | None,
 ):
     """Configure an endpoint
 
@@ -258,7 +267,9 @@ def configure_endpoint(
 
     compute_dir = get_config_dir()
     ep_dir = compute_dir / name
-    Endpoint.configure_endpoint(ep_dir, endpoint_config, multi_user, display_name)
+    Endpoint.configure_endpoint(
+        ep_dir, endpoint_config, multi_user, display_name, auth_policy
+    )
 
 
 @app.command(name="start", help="Start an endpoint")

compute_endpoint/globus_compute_endpoint/endpoint/endpoint.py
@@ -71,12 +71,16 @@ class Endpoint:
         target_path: pathlib.Path,
         multi_user: bool,
         display_name: str | None,
+        auth_policy: str | None,
     ):
         config_text = original_path.read_text()
         config_dict = yaml.safe_load(config_text)
 
         if display_name:
             config_dict["display_name"] = display_name
+
+        if auth_policy:
+            config_dict["authentication_policy"] = auth_policy
 
         if multi_user:
             config_dict["multi_user"] = multi_user
@@ -90,6 +94,7 @@ class Endpoint:
         endpoint_config: pathlib.Path | None = None,
         multi_user=False,
         display_name: str | None = None,
+        auth_policy: str | None = None,
     ):
         """Initialize a clean endpoint dir
 
@@ -98,6 +103,7 @@ class Endpoint:
             the Globus Compute default config file
         :param multi_user: Whether the endpoint is a multi-user endpoint
         :param display_name: A display name to use, if desired
+        :param auth_policy: Globus authentication policy
         """
         log.debug(f"Creating endpoint dir {endpoint_dir}")
         user_umask = os.umask(0o0077)
@@ -120,6 +126,7 @@ class Endpoint:
                 config_target_path,
                 multi_user,
                 display_name,
+                auth_policy,
             )
 
             if multi_user:
@@ -153,6 +160,7 @@ class Endpoint:
         endpoint_config: str | None,
         multi_user: bool = False,
         display_name: str | None = None,
+        auth_policy: str | None = None,
     ):
         ep_name = conf_dir.name
         if conf_dir.exists():
@@ -161,7 +169,9 @@ class Endpoint:
             raise Exception("ConfigExists")
 
         templ_conf_path = pathlib.Path(endpoint_config) if endpoint_config else None
-        Endpoint.init_endpoint_dir(conf_dir, templ_conf_path, multi_user, display_name)
+        Endpoint.init_endpoint_dir(
+            conf_dir, templ_conf_path, multi_user, display_name, auth_policy
+        )
         config_path = Endpoint._config_file_path(conf_dir)
         if multi_user:
             user_conf_tmpl_path = Endpoint.user_config_template_path(conf_dir)
@@ -376,6 +386,7 @@ class Endpoint:
                     multi_user=False,
                     display_name=endpoint_config.display_name,
                     allowed_functions=endpoint_config.allowed_functions,
+                    auth_policy=endpoint_config.authentication_policy,
                 )
 
             except GlobusAPIError as e: