971c68740e98d694b35e1ac1ab21cc5b991915a7
Chris Janidlo <chris@globus.org>
2024-08-08 14:37:07 -0500

    Add functions for retrieving allowed functions

--- compute_sdk/globus_compute_sdk/sdk/client.py
+++ compute_sdk/globus_compute_sdk/sdk/client.py
@@ -743,6 +743,20 @@ class Client:
             raise SystemError(message)
 
     @requires_login
+    def get_allowed_functions(self, endpoint_id):
+        """List the endpoint's whitelist
+        Parameters
+        ----------
+        endpoint_id : str
+            The uuid of the endpoint
+        Returns
+        -------
+        json
+            The response of the request
+        """
+        return self.web_client.get_allowed_functions(endpoint_id)
+
+    @requires_login
     def stop_endpoint(self, endpoint_id: str):
         """Stop an endpoint by dropping it's active connections.
 
