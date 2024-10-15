9ad4e7ba19059f7664736f7088055ac3c65113b2
Reid Mello <30907815+rjmello@users.noreply.github.com>
2024-10-15 19:15:10 -0400

    Bump globus-sdk dependency to at least v3.46.0
    This version moves `GlobusApp` and related objects out of experimental.

--- compute_sdk/setup.py
+++ compute_sdk/setup.py
@@ -7,7 +7,7 @@ from setuptools import find_namespace_packages, setup
 REQUIRES = [
     # request sending and authorization tools
     "requests>=2.31.0,<3",
-    "globus-sdk>=3.45.0,<4",
+    "globus-sdk>=3.46.0,<4",
     "globus-compute-common==0.4.1",
     # dill is an extension of `pickle` to a wider array of native python types
     # pin to the latest version, as 'dill' is not at 1.0 and does not have a clear
