457794bec33605ad09148017a9c15056677293fc
Lei Wang <lei@globus.org>
2025-01-20 15:29:19 -0500

    Add macos version of getting file descriptors in test

--- compute_endpoint/tests/conftest.py
+++ compute_endpoint/tests/conftest.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
+import json
 import os
 import random
 import signal
 import string
 import subprocess
+import sys
 import threading
 import time
 import typing as t
@@ -264,10 +266,29 @@ def htex_warns():
     assert any(_warned(str(w)) for w in pyt_w.list)
 
 
+def get_fds(pid):
+    if sys.platform == "darwin":
+        # Couldn't find timestamp and color equivalents for OSX
+        fd_args = ("/usr/sbin/lsof", "-p", str(pid))
+    else:
+        fd_args = (
+            "/bin/ls",
+            "-lv",
+            "--full-time",
+            "--color=always",
+            f"/proc/{pid}/fd/",
+        )
+    fd_output = subprocess.run(fd_args, capture_output=True)
+
+    # For darwin FDs we normally exclude ' txt ' see
+    # https://stackoverflow.com/questions/795236/in-mac-os-x-how-can-i-get-an-accurate-count-of-file-descriptor-usage # noqa E501
+    # But if doing diffs, do not need to worry about unchanged FDs
+    return fd_output.stdout.decode().split("\n")
+
+
 @pytest.fixture(autouse=True)
 def resource_watcher():
     p = psutil.Process()
-    ls_args = ("/bin/ls", "-lv", "--full-time", "--color=always", f"/proc/{p.pid}/fd/")
     vm_beg = psutil.virtual_memory()
     with p.oneshot():
         mem_beg = p.memory_info()
@@ -275,8 +296,7 @@ def resource_watcher():
         thread_beg = p.num_threads()
         ctx_beg = p.num_ctx_switches()
         io_beg = getattr(p, "io_counters", lambda: "(not supported on this system)")()
-    ls_fds = subprocess.run(ls_args, capture_output=True)
-    os_fds_view_beg = ls_fds.stdout.decode()
+    os_fds_view_beg = get_fds(p.pid)
 
     yield
 
@@ -287,13 +307,14 @@ def resource_watcher():
         thread_end = p.num_threads()
         ctx_end = p.num_ctx_switches()
         io_end = getattr(p, "io_counters", lambda: "(not supported on this system)")()
-    ls_fds = subprocess.run(ls_args, capture_output=True)
-    os_fds_view_end = ls_fds.stdout.decode()
+    os_fds_view_end = get_fds(p.pid)
 
     if fds_end > fds_beg or thread_end > thread_beg:
         thread_list = "\n  ".join(
             f"{i:>3}: {repr(t)}" for i, t in enumerate(threading.enumerate(), start=1)
         )
+        fd_af = [line for line in os_fds_view_end if line not in os_fds_view_beg]
+        fd_be = [line for line in os_fds_view_beg if line not in os_fds_view_end]
         msg = (
             f"\nSystem Virtual Memory:\n  {vm_beg=}\n  {vm_end=}"
             f"\n\nProcess Memory:\n  {mem_beg=}\n  {mem_end=}"
@@ -302,8 +323,8 @@ def resource_watcher():
             f"\n\nI/O Counters:\n  {io_beg=}\n  {io_end=}"
             f"\n\nFile Descriptors:\n  {fds_beg=}\n  {fds_end=}"
             f"\n\nThreads (count: {p.num_threads()}):\n  {thread_list}"
-            f"\n\nOpen files (Before): {os_fds_view_beg}"
-            f"\nOpen files (After): {os_fds_view_end}"
+            f"\n\nOpen files diff before and after: \n"
+            f"{json.dumps(fd_be, indent=2)}\n  ->\n{json.dumps(fd_af, indent=2)}"
         )
         msg = msg.replace("\n", "\n | ")
         assert fds_end <= fds_beg, f"Left over file descriptors!!\n{msg}"
