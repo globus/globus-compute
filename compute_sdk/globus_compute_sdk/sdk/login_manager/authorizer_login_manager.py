funcx-faas/funcx

Deprecate LoginManager and AuthorizerLoginManager
`GlobusApp` objects should be used instead of `LoginManager` objects.

compute_sdk/globus_compute_sdk/sdk/login_manager/authorizer_login_manager.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import logging
+import warnings
 
 import globus_sdk
 from globus_compute_sdk.sdk.login_manager.manager import LoginManager
@@ -22,6 +23,13 @@ class AuthorizerLoginManager(LoginManagerProtocol):
     """
 
     def __init__(self, authorizers: dict[str, globus_sdk.RefreshTokenAuthorizer]):
+        warnings.warn(
+            "The `AuthorizerLoginManager` is deprecated. Please use `GlobusApp` objects"
+            "from the Globus SDK instead:"
+            " https://globus-compute.readthedocs.io/en/stable/sdk.html#globusapps",
+            category=DeprecationWarning,
+            stacklevel=2,
+        )
         self.authorizers = authorizers
 
     def get_auth_client(self) -> globus_sdk.AuthClient:

compute_sdk/tests/unit/test_authorizer_login_manager.py
@@ -32,6 +32,13 @@ def logman(mocker, tmp_path):
     return AuthorizerLoginManager({})
 
 
+def test_authorizer_login_manager_deprecated():
+    with pytest.warns(DeprecationWarning) as record:
+        AuthorizerLoginManager({})
+    msg = "The `AuthorizerLoginManager` is deprecated"
+    assert any(msg in str(r.message) for r in record)
+
+
 def test_auth_client_requires_authorizer_openid_scope(mocker):
     alm = AuthorizerLoginManager({})
     with pytest.raises(KeyError) as pyt_exc:

compute_sdk/tests/unit/test_login_manager.py
@@ -36,6 +36,13 @@ def logman(mocker, tmp_path):
     return LoginManager()
 
 
+def test_login_manager_deprecated():
+    with pytest.warns(DeprecationWarning) as record:
+        LoginManager()
+    msg = "The `LoginManager` is deprecated"
+    assert any(msg in str(r.message) for r in record)
+
+
 def test_is_client_login():
     env = {CID_KEY: "some_id", CSC_KEY: "some_secret"}
     with mock.patch.dict(os.environ, env):

globus/globus-compute

compute_sdk/globus_compute_sdk/sdk/login_manager/authorizer_login_manager.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import logging
+import warnings
 
 import globus_sdk
 from globus_compute_sdk.sdk.login_manager.manager import LoginManager
@@ -22,6 +23,13 @@ class AuthorizerLoginManager(LoginManagerProtocol):
     """
 
     def __init__(self, authorizers: dict[str, globus_sdk.RefreshTokenAuthorizer]):
+        warnings.warn(
+            "The `AuthorizerLoginManager` is deprecated. Please use `GlobusApp` objects"
+            "from the Globus SDK instead:"
+            " https://globus-compute.readthedocs.io/en/stable/sdk.html#globusapps",
+            category=UserWarning,
+            stacklevel=2,
+        )
         self.authorizers = authorizers
 
     def get_auth_client(self) -> globus_sdk.AuthClient:

compute_sdk/tests/unit/test_authorizer_login_manager.py
@@ -32,6 +32,13 @@ def logman(mocker, tmp_path):
     return AuthorizerLoginManager({})
 
 
+def test_authorizer_login_manager_deprecated():
+    with pytest.warns(UserWarning) as record:
+        AuthorizerLoginManager({})
+    msg = "The `AuthorizerLoginManager` is deprecated"
+    assert any(msg in str(r.message) for r in record)
+
+
 def test_auth_client_requires_authorizer_openid_scope(mocker):
     alm = AuthorizerLoginManager({})
     with pytest.raises(KeyError) as pyt_exc:

compute_sdk/tests/unit/test_login_manager.py
@@ -36,6 +36,13 @@ def logman(mocker, tmp_path):
     return LoginManager()
 
 
+def test_login_manager_deprecated():
+    with pytest.warns(UserWarning) as record:
+        LoginManager()
+    msg = "The `LoginManager` is deprecated"
+    assert any(msg in str(r.message) for r in record)
+
+
 def test_is_client_login():
     env = {CID_KEY: "some_id", CSC_KEY: "some_secret"}
     with mock.patch.dict(os.environ, env):