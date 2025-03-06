abfd9217aeb06e7f3f5d7bd9aecdb342b458765d
Kevin Hunter Kesling <kevin@globus.org>
2025-03-05 20:51:56 -0500

    Use v3 register_function API route
    For most users, this will be a silent difference.  However, for users who
    manually serialize and specify the function parts, they will now need to
    specify the function metadata, and, in particular the serialization method.
    
    [sc-36296]

--- compute_sdk/globus_compute_sdk/sdk/executor.py
+++ compute_sdk/globus_compute_sdk/sdk/executor.py
@@ -5,7 +5,6 @@ import copy
 import json
 import logging
 import os
-import platform
 import queue
 import random
 import sys
@@ -20,7 +19,6 @@ from concurrent.futures import InvalidStateError
 import pika
 from globus_compute_common import messagepack
 from globus_compute_common.messagepack.message_types import Result
-from globus_compute_sdk import __version__
 from globus_compute_sdk.errors import TaskExecutionFailed
 from globus_compute_sdk.sdk.asynchronous.compute_future import ComputeFuture
 from globus_compute_sdk.sdk.client import Client
@@ -471,14 +469,7 @@ class Executor(concurrent.futures.Executor):
 
         log.debug("Function not registered. Registering: %s", fn)
         func_register_kwargs.pop("function", None)  # just to be sure
-        reg_kwargs = {
-            "function_name": fn.__name__,
-            "container_uuid": self.container_id,
-            "metadata": {
-                "python_version": platform.python_version(),
-                "sdk_version": __version__,
-            },
-        }
+        reg_kwargs = {"container_uuid": self.container_id}
         reg_kwargs.update(func_register_kwargs)
 
         try:
