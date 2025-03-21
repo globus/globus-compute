6dab050a2864e144111808ed3a7a1104b782c7ea
Yadu Babuji <yadudoc1729@gmail.com>
2025-03-21 10:37:47 -0500

    Update `ShellFunction` to handle changes in the service regarding name limitations
    * ShellFunction(..., name="my_func") sets function name
    * MPIFunction(..., name="my_mpi_func") sets function name
    * ShellFunction name will now default to "ShellFunction" if a name is not specified
    * Invalid names will raise a `ValueError`

--- compute_sdk/tests/unit/test_mpi_functions.py
+++ compute_sdk/tests/unit/test_mpi_functions.py
@@ -1,3 +1,4 @@
+import pytest
 from globus_compute_sdk.sdk.mpi_function import MPIFunction
 
 
@@ -7,3 +8,14 @@ def test_mpi_function(run_in_tmp_dir):
     bash_result = mpi_func()
 
     assert "$PARSL_MPI_PREFIX hostname" in bash_result.cmd
+
+
+def test_mpi_function_name_default(run_in_tmp_dir):
+    mpi_func = MPIFunction("hostname")
+    assert mpi_func.__name__ == type(mpi_func).__name__
+
+
+@pytest.mark.parametrize("fn_name", ["lammps", "my_mpi_app"])
+def test_mpi_function_name(run_in_tmp_dir, fn_name):
+    mpi_func = MPIFunction("hostname", name=fn_name)
+    assert mpi_func.__name__ == fn_name
